cf5c57d2fcb527407f27ec8b8cc36cdd25bb75e5
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-04 18:10:05 +0100

    Update

--- python/mytraits_images.py
+++ python/mytraits_images.py
@@ -10,7 +10,7 @@ def ttr(traitset):
                   "Direct\nreciprocity",
                   "Indirect\nreciprocity",
                   "Fitness"]
-        rows = ["none1", "none2", "i", "none"]
+        rows = ["pi", "p", "i", "none"]
 
     elif traitset == "correlations":
         traits = ["r_qB_Choose",
