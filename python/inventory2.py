afc85e4bcb3e582976a3bd6ea3d4a948d35797f2
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-07 13:34:52 +0100

    Update

--- python/inventory2.py
+++ python/inventory2.py
@@ -72,44 +72,46 @@ def parse_folder_name(folder_name):
         folder_dict["GroupSize"] = 3
     else:
         folder_dict["GroupSize"] = 2
-        folder_dict["Given"] = float(given[-3:]) / 100
-        given_path = os.path.join(mechanism, given)
-        input_files = [f for f in os.listdir(given_path) if f.endswith(input_file_extension)]
-        print(f"{c.white}{mechanism}{c.reset_format}\t{c.white}{given}{c.reset_format}", end = "  ")
-        total_files = len(input_files)
-        if total_files == 0:
-            print(f"{c.bold}{c.red}no {input_file_extension[1:]} files{c.reset_format}")
-            continue
-        with open(os.path.join(given_path, input_files[0]), "r") as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                key, value = row
-                if key == "Given":
-                    if float(value) != folder_dict[key]:
-                        print(f"{c.bold}{c.red}{key} {folder_dict[key]} {value}{c.reset_format}", end = " ")
-                elif key in folder_dict:
-                    if int(value) != folder_dict[key]:
-                        print(f"{c.bold}{c.red}{key} {folder_dict[key]} {value}{c.reset_format}", end = " ")
-        f_smaller_nlines = 0
-        f_equal_nlines = 0
-        f_larger_nlines = 0
-        for f in os.listdir(given_path):
-            if f.endswith(output_file_extension):
-                output_file = os.path.join(given_path, f)
-                with open(output_file, "r") as output:
-                    lines = output.readlines()
-                    if len(lines) < nlines:
-                        f_smaller_nlines += 1
-                    elif len(lines) == nlines:
-                        f_equal_nlines += 1
-                    elif len(lines) > nlines:
-                        f_larger_nlines += 1
-        notstarted = total_files - f_smaller_nlines - f_equal_nlines - f_larger_nlines
-        print(f"{c.bold}{c.green if f_equal_nlines else c.reset_format}{f_equal_nlines:>4}{c.reset_format}" if f_equal_nlines else "", end = "")
-        print(f"{c.bold}{c.yellow if f_smaller_nlines else c.reset_format}{f_smaller_nlines:>4}{c.reset_format}" if f_smaller_nlines else "", end = "")
-        print(f"{c.bold}{c.red if notstarted else c.reset_format}{notstarted:>4}{c.reset_format}" if notstarted else "", end = "")
-        print(f"{c.bold}{c.blue if f_larger_nlines else c.reset_format}{f_larger_nlines:>4}{c.reset_format}" if f_larger_nlines else "", end = "")
-        print()
+
+def process_given_directory(given_path, folder_dict, nlines, input_file_extension, output_file_extension):
+    folder_dict["Given"] = float(given[-3:]) / 100
+    given_path = os.path.join(mechanism, given)
+    input_files = [f for f in os.listdir(given_path) if f.endswith(input_file_extension)]
+    print(f"{c.white}{mechanism}{c.reset_format}\t{c.white}{given}{c.reset_format}", end = "  ")
+    total_files = len(input_files)
+    if total_files == 0:
+        print(f"{c.bold}{c.red}no {input_file_extension[1:]} files{c.reset_format}")
+        return
+    with open(os.path.join(given_path, input_files[0]), "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            key, value = row
+            if key == "Given":
+                if float(value) != folder_dict[key]:
+                    print(f"{c.bold}{c.red}{key} {folder_dict[key]} {value}{c.reset_format}", end = " ")
+            elif key in folder_dict:
+                if int(value) != folder_dict[key]:
+                    print(f"{c.bold}{c.red}{key} {folder_dict[key]} {value}{c.reset_format}", end = " ")
+    f_smaller_nlines = 0
+    f_equal_nlines = 0
+    f_larger_nlines = 0
+    for f in os.listdir(given_path):
+        if f.endswith(output_file_extension):
+            output_file = os.path.join(given_path, f)
+            with open(output_file, "r") as output:
+                lines = output.readlines()
+                if len(lines) < nlines:
+                    f_smaller_nlines += 1
+                elif len(lines) == nlines:
+                    f_equal_nlines += 1
+                elif len(lines) > nlines:
+                    f_larger_nlines += 1
+    notstarted = total_files - f_smaller_nlines - f_equal_nlines - f_larger_nlines
+    print(f"{c.bold}{c.green if f_equal_nlines else c.reset_format}{f_equal_nlines:>4}{c.reset_format}" if f_equal_nlines else "", end = "")
+    print(f"{c.bold}{c.yellow if f_smaller_nlines else c.reset_format}{f_smaller_nlines:>4}{c.reset_format}" if f_smaller_nlines else "", end = "")
+    print(f"{c.bold}{c.red if notstarted else c.reset_format}{notstarted:>4}{c.reset_format}" if notstarted else "", end = "")
+    print(f"{c.bold}{c.blue if f_larger_nlines else c.reset_format}{f_larger_nlines:>4}{c.reset_format}" if f_larger_nlines else "", end = "")
+    print()
 
 def main():
     if len(sys.argv) > 1:
@@ -122,14 +124,13 @@ def main():
     nlines = 10
     input_file_extension = ".glo"
     output_file_extension = ".csv"
-
-    # get the name, not the full path, of the current folder
+    config = Config(nlines, input_file_extension, output_file_extension)
 
     current_folder = os.getcwd()
     variant = current_folder.split("/")[-1]
-    
     mechanisms = [f for f in os.listdir(current_folder) if os.path.isdir(f)]
     mechanisms.sort()
+
     for mechanism in mechanisms:
         givens = [f for f in os.listdir(mechanism) if os.path.isdir(os.path.join(mechanism, f))]
         if len(givens) == 0:
