3a4251c2d9576de6194d20664591a58e9af0b653
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-08 11:54:46 +0100

    Update

--- python/resubmit.py
+++ python/resubmit.py
@@ -1,49 +1,78 @@
 #! /usr/bin/env python
 
 import configparser
+import logging
 import os
 import subprocess
-import logging
-
-# Purpose: resubmit unfinished jobs
-# Usage: python resubmit.py
 
 import mycolors as c
 import myslots
 
+# Purpose: resubmit unfinished jobs
+# Usage: python resubmit.py
+
 queues = ["clk", "epyc"]
-mail_user = "marcelinofuentes@gmail.com"
+
+config_file_path = os.environ.get('CONFIG_FILE')
+if not config_file_path:
+  raise RuntimeError("CONFIG_FILE environment variable not set")
 
 config = configparser.ConfigParser()
-config.read("config.ini")
+config.read(config_file_path)
 
 exe = config.get("DEFAULT", "exe")
-number_of_lines = config.getint("DEFAULT", "number_of_lines")
 hours = config.getint("DEFAULT", "hours")
 memory = config.get("DEFAULT", "memory")
+executable = f"/home/ulc/ba/mfu/code/{exe}/bin/{exe}"
+mail_user = config.get("DEFAULT", "mail_user")
+
 input_file_extension = config.get("DEFAULT", "input_file_extension")
 output_file_extensions = [config.get("DEFAULT", "first_output_file_extension"),
                           config.get("DEFAULT", "second_output_file_extension")]
+number_of_lines = config.getint("DEFAULT", "number_of_lines")
 
-executable = f"/home/ulc/ba/mfu/code/{exe}/bin/{exe}"
 last_job_file = f"/home/ulc/ba/mfu/code/{exe}/results/last_submitted_job.tmp"
 log_file = f"/home/ulc/ba/mfu/code/{exe}/results/submit.log"
 logging.basicConfig(filename=log_file,
                     level=logging.DEBUG,
                     format="%(asctime)s %(levelname)s: %(message)s")
-yesno = f"[{c.bold}{c.green}Yes{c.reset_format}/{c.bold}{c.red}No{c.reset_format}]"
 
-def submit_jobs(free_slots, jobs_to_submit):
+def submit_job(command):
+    output = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+    output = output.stdout.strip()
+    print(output)
+    logging.info(output)
+
+def process_folder(queue, free_slots, jobs_to_submit, test=False):
+    names = [name[:-4] for name in os.listdir() if name.endswith(input_file_extension)]
+    jobs_to_submit = []
+    for name in names:
+        if not os.path.isfile(f"{name}{output_file_extensions[0]}"):
+            jobs_to_submit.append(int(name))
+        else:
+            with open(f"{name}{output_file_extensions[0]}") as f:
+                if sum(1 for line in f) < number_of_lines:
+                    jobs_to_submit.append(int(name))
+
+    if test:
+        print(f"\n{c.bold}Will delete {output_file_extensions} of {jobs_to_submit}?{c.reset_format} "
+    else:
+        for name in jobs_to_submit:
+            for extension in output_file_extensions:
+                if os.path.isfile(f"{name}{extension}"):
+                    os.remove(f"{name}{extension}")
+                else:
+                    continue
+
+    given = os.getcwd()
+    given_folders = given.split("/")
+    given_print = "/".join(given_folders[-3:])
+
     num_jobs_to_submit = min(free_slots, len(jobs_to_submit))
-    job_array = ",".join(map(str, jobs_to_submit[:num_jobs_to_submit]))
-    print(f"\n{c.bold}Submit {job_array}?{c.reset_format} "
-          f"{yesno} ", end="")
-    user_input = input()
-    if user_input.lower() == "n":
-        exit()
     last_job = job_array.split(",")[-1]
     job_name = f"{queue}-{last_job}"
     job_time = f"{hours}:59:00"
+    job_array = ",".join(map(str, jobs_to_submit[:num_jobs_to_submit]))
     command = ["sbatch",
                "--job-name", job_name,
                "--output", f"{job_name}.%j.out",
@@ -56,57 +85,41 @@ def submit_jobs(free_slots, jobs_to_submit):
                "--mail-user", mail_user,
                "--array", job_array,
                "--wrap", f"srun {executable} ${{SLURM_ARRAY_TASK_ID}}"]
-    output = subprocess.run(command, stdout=subprocess.PIPE, text=True)
-    output = output.stdout.strip()
-    print(output)
-    logging.info(output)
-    logging.info(f"{given_print}/{jobs_to_submit[0]}-{jobs_to_submit[num_jobs_to_submit - 1]} to {queue}")
+    if test:
+        print(f"\n{c.bold}Will submit {given_print}/{jobs_to_submit[0]}-{jobs_to_submit[num_jobs_to_submit - 1]} to {queue}{c.reset_format}")
+    else:
+        submit_job(command)
+        logging.info(f"{given_print}/{jobs_to_submit[0]}-{jobs_to_submit[num_jobs_to_submit - 1]} to {queue}")
     del jobs_to_submit[:num_jobs_to_submit]
     free_slots -= num_jobs_to_submit 
 
     return free_slots, jobs_to_submit
 
-names = [name[:-4] for name in os.listdir() if name.endswith(input_file_extension)]
-jobs_to_submit = []
-for name in names:
-    if not os.path.isfile(f"{name}{output_file_extensions[0]}"):
-        jobs_to_submit.append(int(name))
-    else:
-        with open(f"{name}{output_file_extensions[0]}") as f:
-            if sum(1 for line in f) < number_of_lines:
-                jobs_to_submit.append(int(name))
+def main():
 
-print(f"\n{c.bold}Delete {output_file_extensions} of {jobs_to_submit}?{c.reset_format} "
-      f"{yesno} ", end="")
-user_input = input()
-if user_input.lower() == "n":
-    exit()
-for name in jobs_to_submit:
-    for extension in output_file_extensions:
-        if os.path.isfile(f"{name}{extension}"):
-            os.remove(f"{name}{extension}")
-        else:
-            continue
+    test = len(sys.argv) > 1
+    if test:
+        print(f"\n{c.bold}This is a test{c.reset_format}")
 
-given = os.getcwd()
-given_folders = given.split("/")
-given_print = "/".join(given_folders[-3:])
+    for queue in queues:
+        max_submit = myslots.get_max_slots(queue, hours, "maxsubmit")
+        max_running = myslots.get_max_slots(queue, hours, "maxjobspu")
+        running_jobs = myslots.get_slots(queue, "RUNNING")
+        pending_jobs = myslots.get_slots(queue, "PENDING")
+        free_slots = max_submit - running_jobs - pending_jobs
+        if test == True and free_slots == 0:
+            free_slots = 100
+        print(f"\n{c.bold}{queue}{c.reset_format}: {running_jobs} of {max_running} running, "
+              f"{pending_jobs} pending, {c.cyan}{free_slots}{c.reset_format} free")
+        if free_slots > 0 and len(jobs_to_submit) > 0:
+            free_slots, jobs_to_submit = process_folder(free_slots, jobs_to_submit)
 
-for queue in queues:
-    max_submit = myslots.get_max_slots(queue, hours, "maxsubmit")
-    max_running = myslots.get_max_slots(queue, hours, "maxjobspu")
-    running_jobs = myslots.get_slots(queue, "RUNNING")
-    pending_jobs = myslots.get_slots(queue, "PENDING")
-    free_slots = max_submit - running_jobs - pending_jobs
-    print(f"\n{queue}: {running_jobs} of {max_running} running, "
-          f"{pending_jobs} pending, {c.cyan}{free_slots}{c.reset_format} free")
-    if free_slots > 0 and len(jobs_to_submit) > 0:
-        free_slots, jobs_to_submit = submit_jobs(free_slots, jobs_to_submit)
+        print(f"{c.red}{len(jobs_to_submit)} jobs remain to be submitted{c.reset_format}")
+        print(f"{c.cyan}{free_slots}{c.reset_format} free slots")
+        if len(jobs_to_submit) == 0:
+            break
 
-    print(f"{c.red}{len(jobs_to_submit)} jobs remain to be submitted{c.reset_format}")
-    print(f"{c.cyan}{free_slots}{c.reset_format} free slots")
-    if len(jobs_to_submit) == 0:
-        break
+    print()
 
-print()
-
+if __name__ == "__main__":
+    main()
