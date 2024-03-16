86014dcdc5675477b6ded67ab4788a4044b56118
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-16 22:38:33 +0100

    Update

--- python/borra.py
+++ python/borra.py
@@ -1,6 +1,7 @@
 import subprocess
 
 command = ["squeue", "-t", "RUNNING,PENDING", "-r", "-o", f"%j,%K | grep -E 'pi[0-9]+' | grep ,408"]
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = process.communicate()
-print(stdout.decode("utf-8"))
+# print the output of squeue filetered by "pi" followed by a number and 408
+subprocess.run(command)
+
+
