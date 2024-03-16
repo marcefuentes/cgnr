3c8be1dbecea98d488336679fd78d28806e028c1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-16 22:34:58 +0100

    Update

--- python/borra.py
+++ python/borra.py
@@ -1,4 +1,6 @@
 import subprocess
 
 command = ["squeue", "-t", "RUNNING,PENDING", "-r", "-o", f"%j,%K | grep -E 'pi[0-9]+' | grep ,408"]
-out
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = process.communicate()
+print(stdout.decode("utf-8"))
