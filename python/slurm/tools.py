6931b229712c5db3470cdf652d3c14fbc6cc0fc0
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-18 09:46:33 +0100

    Update

--- python/slurm/tools.py
+++ python/slurm/tools.py
@@ -98,7 +98,8 @@ def submit_job(mechanism, last_job, queue, job_array):
         return -1, None, e
 
     executable = f"/home/ulc/ba/mfu/code/{exe}/bin/{exe}"
-    job_name = f"{mechanism}-{last_job}-{queue}"
+    #job_name = f"{mechanism}-{last_job}-{queue}"
+    job_name = f"{queue}-{mechanism}{last_job}"
     job_time = f"{hours}:59:00"
 
     command = ["sbatch",
