f840b44603cc19e82e83118163fd9fb7de8060cd
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-20 12:55:44 +0100

    Update

--- python/tools/colors.py
+++ python/tools/colors.py
@@ -1,13 +1,1 @@
-
-blue         = "\033[94m"
-cyan         = "\033[96m"
-green        = "\033[32m"
-red          = "\033[91m"
-orange       = "\033[33m"
-yellow       = "\033[33m"
-white        = "\033[97m"
-grey         = "\033[90m"
-bold         = "\033[1m"
-reset_format = "\033[0m"
-
-yesno = f"[{bold}{green}Yes{reset_format}/{bold}{red}No{reset_format}]"
+/home/marcelino/bin/colors.py
