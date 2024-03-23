b054fd1664022ddf8c37feeb024d0322a83b55bb
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-23 22:04:10 +0100

    Update

--- python/slots.py
+++ python/slots.py
@@ -14,8 +14,8 @@ for queue in queues:
 
     max_submit = get_qos_limit(queue, "maxsubmit")
     max_running = get_qos_limit(queue, "maxjobspu")
-    running_jobs = get_squeue_stats(queue, "RUNNING")
-    pending_jobs = get_squeue_stats(queue, "PENDING")
+    running_jobs = get_squeue_stats(queue, "running")
+    pending_jobs = get_squeue_stats(queue, "pending")
     free_slots = max_submit - running_jobs - pending_jobs
 
     print(f"{queue:<4} {cc.reset_format} {max_running:>4} {cc.bold}{cc.yellow if running_jobs < max_running else cc.green}{running_jobs:>8}{cc.bold}{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>9}{cc.reset_format} {cc.bold}{cc.cyan}{free_slots:>5}{cc.reset_format}") 
