d4234b53684eac348ef6feb74eae26a774360667
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-23 22:09:28 +0100

    Update

--- python/slots.py
+++ python/slots.py
@@ -7,8 +7,7 @@ import os
 queues = ["clk", "epyc"]
 
 print()
-print(f"{'Queue':<6} {'Max':<4} {'Running':<8} {'Pending':<8} {'Free':<8}")
-#print(f"{'-----':<6} {'---':<4} {'-------':<8} {'-------':<8} {'----':<8}")
+print(f"{'Queue':<7}{'Max':<5}{'Running':<9}{'Pending':<9}{'Free':<9}")
 
 for queue in queues:
 
@@ -18,6 +17,6 @@ for queue in queues:
     pending_jobs = get_squeue_stats(queue, "pending")
     free_slots = max_submit - running_jobs - pending_jobs
 
-    print(f"{queue:<4}{max_running:>6} {cc.bold}{cc.yellow if running_jobs < max_running else cc.green}{running_jobs:>8}{cc.bold}{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>9}{cc.reset_format} {cc.bold}{cc.cyan}{free_slots:>5}{cc.reset_format}") 
+    print(f"{queue:<4}{max_running:>6}{cc.bold}{cc.yellow if running_jobs < max_running else cc.green}{running_jobs:>9}{cc.bold}{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>9}{cc.reset_format} {cc.bold}{cc.cyan}{free_slots:>5}{cc.reset_format}") 
 
 print()
