808875993f053c992f0e8f46d6fcf84377d93593
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-26 09:38:15 +0100

    Update

--- python/slurm/slots.py
+++ python/slurm/slots.py
@@ -2,27 +2,28 @@
 
 import os
 
+import slurm_tools.slurm_tools as st
 import tools.colors as cc
 from tools.get_config import get_config
-from slurm_tools.slurm_tools import get_qos_limit, get_squeue_stats
 
 constraints = get_config("constraints")
 
 print()
-print(f"{'Queue':<7}{'Max':<5}{'Running':<9}{'Pending':<9}{'Free':<9}")
+print(f"{'Queue':<13}{'Max':>5}{'Running':>9}{'Pending':>9}{'Free':>6}")
 
 for constraint in constraints:
 
-    max_submit = get_qos_limit(constraint, "maxsubmit")
-    max_running = get_qos_limit(constraint, "maxjobspu")
-    running_jobs = get_squeue_stats("qos", constraint, "running")
-    pending_jobs = get_squeue_stats("qos", constraint, "pending")
+    qos_name = st.get_qos_name(constraint)
+    max_submit = st.get_qos_limit(constraint, "maxsubmit")
+    max_running = st.get_qos_limit(constraint, "maxjobspu")
+    running_jobs = st.get_squeue_stats("qos", constraint, "running")
+    pending_jobs = st.get_squeue_stats("qos", constraint, "pending")
     free_slots = max_submit - running_jobs - pending_jobs
 
-    print(f"{constraint:<4}", end = "")
-    print(f"{max_running:>6}", end = "")
-    print(f"{cc.bold}{cc.yellow if running_jobs < max_running else cc.green}{running_jobs:>9}{cc.reset}", end = "")
-    print(f"{cc.bold}{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>9}{cc.reset}", end = "")
-    print(f"{cc.bold}{cc.cyan}{free_slots:>5}{cc.reset}")
+    print(f"{qos_name:<13}", end = "")
+    print(f"{max_running:>5}", end = "")
+    print(f"{cc.yellow}{cc.bold if running_jobs == max_running else cc.yellow}{running_jobs:>9}{cc.reset}", end = "")
+    print(f"{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>9}{cc.reset}", end = "")
+    print(f"{cc.bold}{cc.cyan}{free_slots:>6}{cc.reset}")
 
 print()
