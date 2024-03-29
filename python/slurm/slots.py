e9c12c69387e59e3a191e80a33bce9a2408d77b3
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-03-29 21:15:26 +0100

    Update

--- python/slurm/slots.py
+++ python/slurm/slots.py
@@ -18,15 +18,15 @@ def main():
         qos_name = st.get_qos_name(constraint)
         max_submit = st.get_qos_limit(constraint, "maxsubmit")
         max_running = st.get_qos_limit(constraint, "maxjobspu")
-        running_jobs = st.get_squeue_stats("qos", constraint, "running")
-        pending_jobs = st.get_squeue_stats("qos", constraint, "pending")
-        free_slots = max_submit - running_jobs - pending_jobs
+        running = st.get_squeue_stats("qos", constraint, "running")
+        pending = st.get_squeue_stats("qos", constraint, "pending")
+        free_slots = max_submit - running - pending
         total_free_slots += free_slots
 
         print(f"{qos_name:<12}", end = "")
-        print(f"{max_running if max_running > running_jobs else ' ' * 5:>5}", end = "")
-        print(f"{cc.yellow}{cc.bold if running_jobs == max_running else cc.yellow}{running_jobs:>9}{cc.reset}", end = "")
-        print(f"{cc.red if pending_jobs == 0 else cc.white}{pending_jobs:>4}{cc.reset}", end = "")
+        print(f"({max_running if max_running > running else ' ' * 5:>5})", end = "")
+        print(f"{cc.yellow}{cc.bold if running == max_running else cc.yellow}{running:>9}{cc.reset}", end = "")
+        print(f"{cc.red if pending == 0 else cc.white}{pending:>4}{cc.reset}", end = "")
         print(f"{cc.bold}{cc.cyan}{free_slots if free_slots else '':>4}{cc.reset}")
 
     if total_free_slots:
