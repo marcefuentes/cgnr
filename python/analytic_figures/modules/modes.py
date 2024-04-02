d787928692d291fed8f682d0c248d4c7de0029e0
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-02 10:07:47 +0200

    Update

--- python/analytic_figures/modules/modes.py
+++ python/analytic_figures/modules/modes.py
@@ -14,51 +14,67 @@ title = {
 }
 
 traits = {
-    "cooperation":          ["ChooseGrain",
-                             "MimicGrain",
-                             "ImimicGrain",
-                             "qBSeen",
-                             "w"],
-    "cooperationlt":        ["ChooseGrain",
-                             "Choose_ltGrain",
-                             "MimicGrain",
-                             "ImimicGrain",
-                             "Imimic_ltGrain",
-                             "qBSeen",
-                             "w"],
-    "correlationsqB":       ["r_qB_Choose",
-                             "r_qB_Mimic",
-                             "r_qB_Imimic"],
-    "correlationsqBlt":     ["r_qB_Choose",
-                             "r_qB_Choose_lt",
-                             "r_qB_Mimic",
-                             "r_qB_Imimic",
-                             "r_qB_Imimic_lt"],
-    "correlations":         ["r_Choose_Mimic",
-                             "r_Choose_Imimic",
-                             "r_Mimic_Imimic"],
-    "correlationsmlt":      ["r_Choose_Choose_lt",
-                             "r_Choose_Mimic",
-                             "r_Choose_Imimic",
-                             "r_Choose_Imimic_lt",
-                             "r_Choose_lt_Mimic",
-                             "r_Choose_lt_Imimic",
-                             "r_Choose_lt_Imimic_lt",
-                             "r_Mimic_Imimic",
-                             "r_Mimic_Imimic_lt",
-                             "r_Imimic_Imimic_lt"],
-    "none":                 ["qBSeen",
-                             "qBSeen_byproduct",
-                             "w",
-                             "w_deficit"],
-    "test":                 ["qBSeen",
-                             "w"]
+    "cooperation": [
+        "ChooseGrain",
+        "MimicGrain",
+        "ImimicGrain",
+        "qBSeen",
+        "w"
+    ],
+    "cooperationlt": [
+        "ChooseGrain",
+        "Choose_ltGrain",
+        "MimicGrain",
+        "ImimicGrain",
+        "Imimic_ltGrain",
+        "qBSeen",
+        "w"
+    ],
+    "correlationsqB": [
+        "r_qB_Choose",
+        "r_qB_Mimic",
+        "r_qB_Imimic"
+    ],
+    "correlationsqBlt": [
+        "r_qB_Choose",
+        "r_qB_Choose_lt",
+        "r_qB_Mimic",
+        "r_qB_Imimic",
+        "r_qB_Imimic_lt"
+    ],
+    "correlations": [
+        "r_Choose_Mimic",
+        "r_Choose_Imimic",
+        "r_Mimic_Imimic"
+    ],
+    "correlationsmlt": [
+        "r_Choose_Choose_lt",
+        "r_Choose_Mimic",
+        "r_Choose_Imimic",
+        "r_Choose_Imimic_lt",
+        "r_Choose_lt_Mimic",
+        "r_Choose_lt_Imimic",
+        "r_Choose_lt_Imimic_lt",
+        "r_Mimic_Imimic",
+        "r_Mimic_Imimic_lt",
+        "r_Imimic_Imimic_lt"
+    ],
+    "none": [
+        "qBSeen",
+        "qBSeen_byproduct",
+        "w",
+        "w_deficit"
+    ],
+    "test": [
+        "qBSeen",
+        "w"
+    ]
 }
 
 mechanisms = {
-    "default":              ["pi", "p", "i", "none"],
-    "none":                 ["given100", "given095", "given050", "given000"],
-    "test":                 ["p", "i", "none"]
+    "default":  ["pi", "p", "i", "none"],
+    "none":     ["given100", "given095", "given050", "given000"],
+    "test":     ["p", "i", "none"]
 }
 
 def get_title(trait):
