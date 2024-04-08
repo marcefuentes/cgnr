caa00a81caee9d97358e1bbf6f215a67671fd4f4
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-08 18:17:56 +0200

    Update

---
+++ python/slurm/modules/list_of_folders.py
@@ -0,0 +1,14 @@
+# Description: This module contains a function that returns
+# a sorted list of all the folders in a given directory.
+
+import os
+
+def list_of_folders(path):
+    folders = []
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            folders.append(item_path)
+    folders.sort()
+    return folders
+
