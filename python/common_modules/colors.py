124daaf1c8163189801216553a42098413a881b3
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-08 18:42:08 +0200

    Update

---
+++ python/common_modules/colors.py
@@ -0,0 +1,14 @@
+
+blue    = "\033[94m"
+cyan    = "\033[96m"
+green   = "\033[32m"
+red     = "\033[91m"
+purple  = "\033[95m"
+yellow  = "\033[33m"
+white   = "\033[97m"
+grey    = "\033[90m"
+bold    = "\033[1m"
+reset   = "\033[0m"
+
+yesno = f"[{bold}{green}Yes{reset}/{bold}{red}No{reset}]"
+noyes = f"[{bold}{red}Yes{reset}/{bold}{green}No{reset}]"
