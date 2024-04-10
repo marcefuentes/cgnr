f0fcae05485fd1e2290f68a10d1fc58e747945af
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-10 11:40:20 +0200

    Update

---
+++ python/analytic_figures/modules/update_zmatrix.py
@@ -0,0 +1,45 @@
+
+import numpy as np
+import pandas as pd
+
+def get_zmatrix(t, df, trait):
+    m = df.Time == t
+    zmatrix = pd.pivot(df.loc[m],
+        values=trait,
+        index="alpha",
+        columns="logES")
+    zmatrix = zmatrix.sort_index(axis=0, ascending=False)
+    zmatrix = zmatrix.to_numpy()
+    return zmatrix
+
+def update_zmatrix(t, df_dict, mechanism, trait, mode):
+    if "nothing" in trait:
+        zmatrix = np.zeros((1, 1))
+    elif "r_" in trait:
+        zmatrix = get_zmatrix(t, df_dict[mechanism], trait)
+    elif "byproduct" in trait:
+        zmatrix = get_zmatrix(t, df_dict[mechanism], "qBmean")
+    elif "deficit" in trait:
+        zmatrix = get_zmatrix(t, df_dict[mechanism], "wmean")
+    else:
+        zmatrix = get_zmatrix(t, df_dict[mechanism], f"{trait}mean")
+    if "cooperation" in mode:
+        if "Grain" in trait:
+            if mechanism == "none":
+                zmatrix = 0.5 - zmatrix
+            else:
+                zmatrix = get_zmatrix(t, df_dict["none"], f"{trait}mean") - zmatrix
+        else:
+            zmatrix = zmatrix - get_zmatrix(t, df_dict["social"], f"{trait}mean")
+    elif mode == "none":
+        if "byproduct" in trait:
+            given = df_dict[mechanism]["Given"].iloc[0]
+            zmatrix = zmatrix * given
+        elif "deficit" in trait:
+            zmatrix = zmatrix - get_zmatrix(t, df_dict["social"], "wmean")
+    elif mode == "demography":
+        if "Dispersal" in trait:
+            zmatrix = zmatrix - get_zmatrix(t, df_dict[mechanism], "NeutralDispersalRatemean")
+        elif "N" in trait:
+            zmatrix = zmatrix/4096.0
+    return zmatrix
