2452907c70b246b26508c1bab4ce80e85fca3921
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-10 19:30:52 +0200

    Update

--- python/slurm/submit.py
+++ python/slurm/submit.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python
 
+""" Submit jobs. """
+
 import argparse
 import logging
 import os
 import sys
 
-import modules.slurm_tools as st
 import common_modules.colors as cc
-from modules.list_of_folders import list_of_folders
 from common_modules.get_config import get_config
+import modules.slurm_tools as st
+from modules.list_of_folders import list_of_folders
 
 # Purpose: browse through folders and submit jobs
 # Usage: python submit.py or python submit.py test
 
 def get_job_min(current_path):
+    """ Get the minimum job number in the current folder. """
     input_file_extension = get_config("input_file_extension")
     job_min = 9999
     for file in os.listdir(current_path):
         if file.endswith(input_file_extension):
             basename = int(file.split(".")[0])
-            if basename < job_min:
-                job_min = basename
+            job_min = min(job_min, basename)
     return job_min
 
 def get_job_max(current_path):
+    """ Get the maximum job number in the current folder. """
     input_file_extension = get_config("input_file_extension")
     job_max = 0
     for file in os.listdir(current_path):
         if file.endswith(input_file_extension):
             basename = int(file.split(".")[0])
-            if basename > job_max:
-                job_max = basename
+            job_max = max(job_max, basename)
     return job_max
 
 def process_folder(constraint, free_slots, last_job, test):
+    """ Process the folder with input files. """
     output_file_extension, *_ = get_config("output_file_extensions")
     current_path = os.getcwd()
     current_path_folders = current_path.split("/")
@@ -43,7 +46,11 @@ def process_folder(constraint, free_slots, last_job, test):
     else:
         job_min = last_job + 1
     if os.path.isfile(os.path.join(current_path, f"{job_min}{output_file_extension}")):
-        print(f"{cc.red}{current_path_print}/{job_min}{output_file_extension} already exists.{cc.reset}")
+        msg = (
+            f"{cc.red}{current_path_print}/{job_min}{output_file_extension} "
+            f"already exists.{cc.reset}"
+        )
+        print(msg)
         last_job = 0
         return free_slots, last_job
     job_max = get_job_max(current_path)
@@ -57,13 +64,17 @@ def process_folder(constraint, free_slots, last_job, test):
         stdout = "Test"
         stderr = "Test"
     else:
-        return_code, stdout, stderr = st.submit_job(current_path_folders, job_array_string, constraint)
+        return_code, stdout, stderr = st.submit_job(
+            current_path_folders,
+            job_array_string,
+            constraint
+        )
     if return_code != 0:
         print(f"{cc.red}sbatch command failed with return code {return_code}.{cc.reset}")
         if stderr:
             print(stderr)
             logging.error(stderr)
-        exit()
+        sys.exit()
     else:
         for line in stdout.split("\n"):
             if line:
@@ -77,14 +88,15 @@ def process_folder(constraint, free_slots, last_job, test):
     return free_slots, last_job
 
 def process_variant(constraint, free_slots, test, last_job_file):
+    """ Process the parent directory. """
     if os.path.isfile(last_job_file):
-        with open(last_job_file, "r") as f:
+        with open(last_job_file, "r", encoding="utf-8") as f:
             current_path, last_job = f.read().strip().split(",")
         last_job = int(last_job)
     else:
         if test:
             print("Submission is about to start in a new variant. Cannot run with --test option.")
-            exit()
+            sys.exit()
         mechanisms = list_of_folders(os.getcwd())
         givens = list_of_folders(mechanisms[0])
         current_path = givens[0]
@@ -93,12 +105,12 @@ def process_variant(constraint, free_slots, test, last_job_file):
         print(f"\n{cc.bold}Submit jobs in {current_path_print}?{cc.reset} {cc.yesno} ", end="")
         user_input = input()
         if user_input.lower() == "n":
-            exit()
+            sys.exit()
         last_job = 0
     os.chdir(current_path)
     free_slots, last_job = process_folder(constraint, free_slots, last_job, test)
 
-    if last_job == 0: 
+    if last_job == 0:
         mechanism = os.path.dirname(current_path)
         givens = list_of_folders(mechanism)
         given_index = givens.index(current_path) + 1
@@ -117,20 +129,24 @@ def process_variant(constraint, free_slots, test, last_job_file):
                 else:
                     os.remove(last_job_file)
                 print(f"{cc.bold}{cc.green}All jobs submitted.{cc.reset}")
-                print(f"{cc.bold}{cc.cyan}{free_slots}{cc.reset} free slots in {cc.bold}{constraint}{cc.reset}.\n")
-                exit()
+                msg = (
+                    f"{cc.bold}{cc.cyan}{free_slots}{cc.reset} "
+                    f"free slots in {cc.bold}{constraint}{cc.reset}.\n"
+                )
+                print(msg)
+                sys.exit()
     if test:
         print(f"Would write {current_path},{last_job} to {last_job_file}.{cc.reset}")
     else:
-        with open(last_job_file, "w") as f:
+        with open(last_job_file, "w", encoding="utf-8") as f:
             f.write(f"{current_path},{last_job}")
 
     return free_slots
 
 def main(test=False):
-
+    """ Main function. """
     if test:
-        print(f"\nThis is a test.")
+        print("\nThis is a test.")
     exe = get_config("exe")
     last_job_file = f"/home/ulc/ba/mfu/code/{exe}/results/last_submitted_job.tmp"
     log_file = f"/home/ulc/ba/mfu/code/{exe}/results/submit.log"
