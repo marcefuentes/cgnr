43d2278ac782abdc3696c6cf534b8b80267675a1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-12 13:37:03 +0200

    Update

--- python/common_modules/get_config.py
+++ python/common_modules/get_config.py
@@ -4,7 +4,7 @@
 import configparser
 import os
 
-import common_modules.color
+import common_modules.color as color
 
 def get_config(variable):
     """ Reads the config file and returns the value of the variable passed as argument """
--- python/slurm/submit2.py
+++ python/slurm/submit2.py
@@ -5,7 +5,7 @@
 import os
 import sys
 
-import common_modules.color
+import common_modules.color as color
 from common_modules.get_config import get_config
 from modules.argparse_utils import parse_args
 from modules.list_of_folders import list_of_folders
