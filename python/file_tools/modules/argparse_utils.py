0c7eb415b6748657ddb6059d70265b61b12a84b0
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-13 23:19:25 +0200

    Update

---
+++ python/file_tools/modules/argparse_utils.py
@@ -0,0 +1,68 @@
+
+""" Parses the arguments of the command line. """
+
+import argparse
+
+from common_modules.get_config import get_config
+
+def parse_args(description):
+    """Parse command line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description=f"description: {description}",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    default_groupsize = get_config("GroupSize")
+    default_cost = get_config("Cost")
+    default_given = get_config("Given")
+
+    parser.add_argument(
+        "--groupsize",
+        type=int,
+        default=default_groupsize,
+        help="group size"
+    )
+    parser.add_argument(
+        "--cost",
+        type=float,
+        default=default_cost,
+        help="cost value"
+    )
+    parser.add_argument(
+        "--partnerchoice",
+        action="store_const",
+        const=1,
+        default=0,
+        help="enable partnerchoice"
+    )
+    parser.add_argument(
+        "--indirectr",
+        action="store_const",
+        const=1,
+        default=0,
+        help="enable indirectr"
+    )
+    parser.add_argument(
+        "--language",
+        action="store_const",
+        const=1,
+        default=0,
+        help="enable language"
+    )
+    parser.add_argument(
+        "--shuffle",
+        action="store_const",
+        const=1,
+        default=0,
+        help="enable shuffle"
+    )
+    parser.add_argument(
+        "--given",
+        type=float,
+        default=default_given,
+        help=f"given value"
+    )
+
+    args = parser.parse_args()
+    return args
