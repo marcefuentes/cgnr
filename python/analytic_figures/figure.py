541efcbaffc962b8a9b9e0da0a71dc2ccdc3f286
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-16 16:32:17 +0200

    Update

--- python/analytic_figures/figure.py
+++ python/analytic_figures/figure.py
@@ -19,8 +19,9 @@ from modules.prettify_axes import prettify_imshow_axes, prettify_plot_axes
 from modules.prettify_fig import prettify_fig, create_measurements
 from modules.update import update
 
+
 def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
-    """ Create the figure. """
+    """Create the figure."""
 
     start_time = time.perf_counter()
     this_script = os.path.basename(__file__)
@@ -44,7 +45,9 @@ def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
     row_titles = []
 
     if mode_is_single_trait:
-        rows = mm.dict_single_trait_rows_1.get(mode, mm.dict_single_trait_rows_1["default"])
+        rows = mm.dict_single_trait_rows_1.get(
+            mode, mm.dict_single_trait_rows_1["default"]
+        )
         columns = mm.dict_traits[mode]["variants"]
         for column in columns:
             column_titles.append(mm.dict_variant_titles[column])
@@ -61,34 +64,15 @@ def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
     measurements = create_measurements(nrows, ncols)
 
     if histogram:
-        fig, axs, divider = create_fig(
-            measurements,
-            nrows,
-            ncols,
-            nc=nc,
-            nr=nr
-        )
+        fig, axs, divider = create_fig(measurements, nrows, ncols, nc=nc, nr=nr)
         axs = prettify_plot_axes(
-            axs,
-            divider,
-            alphas,
-            logess,
-            row_titles,
-            column_titles
+            axs, divider, alphas, logess, row_titles, column_titles
         )
         artists = init_plot_artists(axs)
     else:
-        fig, axs, divider = create_fig(
-            measurements,
-            nrows,
-            ncols)
+        fig, axs, divider = create_fig(measurements, nrows, ncols)
         axs = prettify_imshow_axes(
-            axs,
-            divider,
-            alphas,
-            logess,
-            row_titles,
-            column_titles
+            axs, divider, alphas, logess, row_titles, column_titles
         )
         artists = init_imshow_artists(axs, nr, nc)
 
@@ -102,17 +86,17 @@ def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
         name = f"{name}_histogram"
 
     dict_update = {
-        "mode":                 mode,
+        "mode": mode,
         "mode_is_single_trait": mode_is_single_trait,
-        "columns":              columns,
-        "rows":                 rows,
-        "dfs":                  dfs,
-        "df_none":              df_none,
-        "df_social":            df_social,
-        "dffrqs":               dffrqs,
-        "movie":                movie,
-        "text":                 fig.texts[2],
-        "artists":              artists
+        "columns": columns,
+        "rows": rows,
+        "dfs": dfs,
+        "df_none": df_none,
+        "df_social": df_social,
+        "dffrqs": dffrqs,
+        "movie": movie,
+        "text": fig.texts[2],
+        "artists": artists,
     }
     if histogram:
         dict_update["alphas"] = alphas
@@ -120,28 +104,25 @@ def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
 
     if movie:
         dict_animation = {
-            "fig":      fig,
-            "frames":   ts,
-            "func":     update,
-            "fargs":    (dict_update,),
-            "blit":     True
+            "fig": fig,
+            "frames": ts,
+            "func": update,
+            "fargs": (dict_update,),
+            "blit": True,
         }
         ani = FuncAnimation(**dict_animation)
         ani.save(f"{name}.mp4", writer="ffmpeg", fps=10)
     else:
         if mode in ("all_lang_traits", "all_traits"):
             if mode == "all_lang_traits":
-                traits = [
-                    "Choose_ltGrain",
-                    "Imimic_ltGrain"
-                ]
+                traits = ["Choose_ltGrain", "Imimic_ltGrain"]
             else:
                 traits = [
                     "ChooseGrain",
                     "MimicGrain",
                     "ImimicGrain",
                     "w_excess",
-                    "qBSeen_excess"
+                    "qBSeen_excess",
                 ]
             for trait in traits:
                 dict_update["mode"] = trait
@@ -155,11 +136,12 @@ def main(mode, histogram=False, movie=False, mode_is_single_trait=False):
     end_time = time.perf_counter()
     print(f"\nTime elapsed: {(end_time - start_time):.2f} seconds")
 
+
 if __name__ == "__main__":
     args, is_trait = parse_args()
     main(
         mode=args.mode,
         histogram=args.histogram,
         movie=args.movie,
-        mode_is_single_trait=is_trait
+        mode_is_single_trait=is_trait,
     )
