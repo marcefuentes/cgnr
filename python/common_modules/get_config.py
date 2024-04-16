2f9aa43d0e7f5c4437ae44cc9b6427019483f792
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-16 16:52:15 +0200

    Update

--- python/common_modules/get_config.py
+++ python/common_modules/get_config.py
@@ -1,15 +1,17 @@
-
 """ Reads the config file and returns the value of the variable passed as argument """
 
 import configparser
 import os
 
-import common_modules.color as color
+from common_modules import color
+
 
 def get_config(variable):
-    """ Reads the config file and returns the value of the variable passed as argument """
+    """Reads the config file and returns the value of the variable passed as argument"""
 
-    config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../config.ini")
+    config_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../../config.ini"
+    )
     if not config_file_path:
         return RuntimeError("config.ini does not exist in the root of the project")
 
@@ -27,27 +29,20 @@ def get_config(variable):
         "DeathRate",
         "GroupSize",
         "Cost",
-        "grid"
+        "grid",
     ]
-    float_variables = [
-        "alpha_min",
-        "alpha_max",
-        "loges_min",
-        "loges_max",
-        "Given"
-    ]
-    list_variables = [
-        "output_file_extensions",
-        "constraints"
-    ]
+    float_variables = ["alpha_min", "alpha_max", "loges_min", "loges_max", "Given"]
+    list_variables = ["output_file_extensions", "constraints"]
 
-    if config.has_option('DEFAULT', variable):
+    if config.has_option("DEFAULT", variable):
         if variable in int_variables:
-            return config.getint('DEFAULT', variable)
+            return config.getint("DEFAULT", variable)
         if variable in float_variables:
-            return config.getfloat('DEFAULT', variable)
+            return config.getfloat("DEFAULT", variable)
         if variable in list_variables:
             return config.get("DEFAULT", variable).split(",")
-        return config.get('DEFAULT', variable)
-    print(f"{color.BOLD}{color.RED}{variable} not found in {config_file_path}{color.RESET}")
+        return config.get("DEFAULT", variable)
+    print(
+        f"{color.BOLD}{color.RED}{variable} not found in {config_file_path}{color.RESET}"
+    )
     return 0
