3b58e772e080d9718b0c6fab214a89fe491c98b5
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-20 11:30:20 +0200

    Update

--- python/slurm/submit.py
+++ python/slurm/submit.py
@@ -16,54 +16,70 @@ import modules.slurm_tools as st
 # Usage: python submit.py or python submit.py test
 
 
-def get_job_max(current_path):
-    """Get the maximum job number in the current folder."""
+def get_job_max(work_path):
+    """Get the maximum job number in the work folder."""
 
     input_file_extension = get_config("input_file_extension")
     job_max = 0
-    for file in os.listdir(current_path):
+    for file in os.listdir(work_path):
         if file.endswith(input_file_extension):
             basename = int(file.split(".")[0])
             job_max = max(job_max, basename)
     return job_max
 
 
-def get_job_min(current_path):
-    """Get the minimum job number in the current folder."""
+def get_job_min(work_path):
+    """Get the minimum job number in the work folder."""
 
     input_file_extension = get_config("input_file_extension")
     job_min = 9999
-    for file in os.listdir(current_path):
+    for file in os.listdir(work_path):
         if file.endswith(input_file_extension):
             basename = int(file.split(".")[0])
             job_min = min(job_min, basename)
     return job_min
 
 
+def next_work_path(work_path):
+
+    mechanism = os.path.dirname(work_path)
+    givens = list_of_folders(mechanism)
+    given_index = givens.index(work_path) + 1
+    if given_index < len(givens):
+        return False, givens[given_index]
+    mechanisms = list_of_folders(os.path.dirname(mechanism))
+    mechanism_index = mechanisms.index(mechanism) + 1
+    if mechanism_index < len(mechanisms):
+        mechanism = mechanisms[mechanism_index]
+        givens = list_of_folders(mechanism)
+        return False, givens[0]
+    return True, work_path
+
+
 def process_folder(constraint, free_slots, last_job, test):
-    """Submit jobs in the current folder"""
+    """Submit jobs in the work folder"""
 
     output_file_extension, *_ = get_config("output_file_extensions")
-    current_path = os.getcwd()
-    current_path_folders = current_path.split("/")
-    current_path_print = "/".join(current_path_folders[-3:])
+    work_path = os.getcwd()
+    work_path_folders = work_path.split("/")
+    work_path_print = "/".join(work_path_folders[-3:])
     if last_job == 0:
-        job_min = get_job_min(current_path)
+        job_min = get_job_min(work_path)
     else:
         job_min = last_job + 1
-    if os.path.isfile(os.path.join(current_path, f"{job_min}{output_file_extension}")):
+    if os.path.isfile(os.path.join(work_path, f"{job_min}{output_file_extension}")):
         msg = (
-            f"{color.RED}{current_path_print}/{job_min}{output_file_extension} "
+            f"{color.RED}{work_path_print}/{job_min}{output_file_extension} "
             f"already exists.{color.RESET}"
         )
         print(msg)
         last_job = 0
         return free_slots, last_job
-    job_max = get_job_max(current_path)
+    job_max = get_job_max(work_path)
     num_jobs_to_submit = min(free_slots, job_max - job_min + 1)
     last_job = job_min + num_jobs_to_submit - 1
     job_array_string = f"{job_min}-{last_job}"
-    process_jobs(current_path_folders, job_array_string, constraint, test)
+    process_jobs(work_path_folders, job_array_string, constraint, test)
     free_slots -= num_jobs_to_submit
     if last_job == job_max:
         last_job = 0
@@ -75,7 +91,7 @@ def process_variant(constraint, free_slots, test, last_job_file):
 
     if os.path.isfile(last_job_file):
         with open(last_job_file, "r", encoding="utf-8") as f:
-            current_path, last_job = f.read().strip().split(",")
+            work_path, last_job = f.read().strip().split(",")
         last_job = int(last_job)
     else:
         if test:
@@ -85,49 +101,38 @@ def process_variant(constraint, free_slots, test, last_job_file):
             sys.exit()
         mechanisms = list_of_folders(os.getcwd())
         givens = list_of_folders(mechanisms[0])
-        current_path = givens[0]
-        current_path_folders = current_path.split("/")
-        current_path_print = "/".join(current_path_folders[-3:])
+        work_path = givens[0]
+        work_path_folders = work_path.split("/")
+        work_path_print = "/".join(work_path_folders[-3:])
         print(
-            f"\n{color.BOLD}Submit jobs in {current_path_print}?{color.RESET} {color.YESNO} ",
+            f"\n{color.BOLD}Submit jobs in {work_path_print}?{color.RESET} {color.YESNO} ",
             end="",
         )
         user_input = input()
         if user_input.lower() == "n":
             sys.exit()
         last_job = 0
-    os.chdir(current_path)
+    os.chdir(work_path)
     free_slots, last_job = process_folder(constraint, free_slots, last_job, test)
 
     if last_job == 0:
-        mechanism = os.path.dirname(current_path)
-        givens = list_of_folders(mechanism)
-        given_index = givens.index(current_path) + 1
-        if given_index < len(givens):
-            current_path = givens[given_index]
-        else:
-            mechanisms = list_of_folders(os.path.dirname(mechanism))
-            mechanism_index = mechanisms.index(mechanism) + 1
-            if mechanism_index < len(mechanisms):
-                mechanism = mechanisms[mechanism_index]
-                givens = list_of_folders(mechanism)
-                current_path = givens[0]
+        all_submitted, work_path = next_work_path(work_path)
+        if all_submitted:
+            if test:
+                print(f"Would remove {last_job_file}.{color.RESET}")
             else:
-                if test:
-                    print(f"Would remove {last_job_file}.{color.RESET}")
-                else:
-                    os.remove(last_job_file)
-                print(
-                    f"{color.BOLD}{color.GREEN}All jobs submitted{color.RESET}\n"
-                    + f"{color.BOLD}{color.CYAN}{free_slots}{color.RESET} "
-                    + f"free slots in {color.BOLD}{constraint}{color.RESET}\n"
-                )
-                sys.exit()
+                os.remove(last_job_file)
+            print(
+                f"{color.BOLD}{color.GREEN}All jobs submitted{color.RESET}\n"
+                + f"{color.BOLD}{color.CYAN}{free_slots}{color.RESET} "
+                + f"free slots in {color.BOLD}{constraint}{color.RESET}\n"
+            )
+            sys.exit()
     if test:
-        print(f"Would write {current_path},{last_job} to {last_job_file}.{color.RESET}")
+        print(f"Would write {work_path},{last_job} to {last_job_file}.{color.RESET}")
     else:
         with open(last_job_file, "w", encoding="utf-8") as f:
-            f.write(f"{current_path},{last_job}")
+            f.write(f"{work_path},{last_job}")
 
     return free_slots
 
@@ -143,8 +148,8 @@ def main(test=False):
     for constraint in constraints:
         free_slots = st.get_free_slots(constraint)
         print(
-                f"\n{color.BOLD}{constraint}:{color.RESET}"
-                + f"{color.CYAN}{free_slots}{color.RESET} free slots"
+            f"\n{color.BOLD}{constraint}:{color.RESET}"
+            + f"{color.CYAN}{free_slots}{color.RESET} free slots"
         )
         if test and not free_slots:
             free_slots = 100
