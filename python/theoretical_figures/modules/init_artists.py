aa230137c69a5767cc5304b8709cd82d83453aa2
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-22 00:41:52 +0200

    Update

--- python/theoretical_figures/modules/init_artists.py
+++ python/theoretical_figures/modules/init_artists.py
@@ -13,18 +13,18 @@ def init_plot_artists(axs, update_args):
     dummy_budgety = np.full_like(update_args["icx"], -1.0)
     dummy_icy = np.zeros_like(update_args["icx"])
 
-    for g in range(2):
-        for a, alpha in enumerate(update_args["alphas"]):
-            for r, rho in enumerate(update_args["rhos"]):
-                if g == 0:
+    for j in range(2):
+        for k, alpha in enumerate(update_args["alphas"]):
+            for m, rho in enumerate(update_args["rhos"]):
+                if j == 0:
                     for c in range(ss.N_IC): 
-                        axs[0, a, r].plot(update_args["icx"], update_args["isoclines"][a, r, c], c="0.850")
-                budgets[g, a, r], = axs[g, a, r].plot(update_args["icx"],
+                        axs[0, k, m].plot(update_args["icx"], update_args["isoclines"][k, m, c], c="0.850")
+                budgets[j, k, m], = axs[j, k, m].plot(update_args["icx"],
                     dummy_budgety,
                     c="0.300",
                     linewidth=4,
                     alpha=0.8)
-                icurves[g, a, r], = axs[g, a, r].plot(update_args["icx"],
+                icurves[j, k, m], = axs[j, k, m].plot(update_args["icx"],
                     dummy_icy,
                     linewidth=4,
                     alpha=0.8)
