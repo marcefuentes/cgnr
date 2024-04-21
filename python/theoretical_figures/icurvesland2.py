179c9ed370aca8dd942324d8f0f2ba1f916c738b
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-21 11:15:16 +0200

    Update

--- python/theoretical_figures/icurvesland2.py
+++ python/theoretical_figures/icurvesland2.py
@@ -30,9 +30,9 @@ plotsize = 6
 
 # Add data to figure
 
-def update(given, budgets, icurves):
+def update(given, budgets, icurves, alphas, rhos, icx, budget_0):
 
-    budget_own = budget0*(1.0 - given)
+    budget_own = budget_0*(1.0 - given)
 
     for a, alpha in enumerate(alphas):
         for r, rho in enumerate(rhos):
@@ -53,151 +53,156 @@ def update(given, budgets, icurves):
 
     return np.concatenate([budgets.flatten(), icurves.flatten()])
 
-# Data
 
-ALPHA_MIN = get_config("alpha_min")
-ALPHA_MAX = get_config("alpha_max")
-LOGES_MIN = get_config("loges_min")
-LOGES_MAX = get_config("loges_max")
-alphas = np.linspace(ALPHA_MAX, ALPHA_MIN, num=num)
-logess = np.linspace(LOGES_MIN, LOGES_MAX, num=num)
-rhos = 1.0 - 1.0/pow(2, logess)
-icx = np.linspace(0.001,
-                  0.999,
-                  num=numqB)
-budget0 = 1.0 - icx
-ws = np.linspace(1.0/(n_ic + 1), n_ic/(n_ic + 1), num=n_ic)
-ics = np.zeros((num, num, n_ic, numqB))
-for i, alpha in enumerate(alphas):
-    for j, rho in enumerate(rhos):
-        for k, w in enumerate(ws):
-            ics[i, j, k] = tt.indifference(icx, w, alpha, rho)
-norm = Normalize(vmin=0, vmax=1)
+def main():
+    # Data
 
-# Figure properties
+    ALPHA_MIN = get_config("alpha_min")
+    ALPHA_MAX = get_config("alpha_max")
+    LOGES_MIN = get_config("loges_min")
+    LOGES_MAX = get_config("loges_max")
+    alphas = np.linspace(ALPHA_MAX, ALPHA_MIN, num=num)
+    logess = np.linspace(LOGES_MIN, LOGES_MAX, num=num)
+    rhos = 1.0 - 1.0/pow(2, logess)
+    icx = np.linspace(0.001,
+                      0.999,
+                      num=numqB)
+    budget_0 = 1.0 - icx
+    ws = np.linspace(1.0/(n_ic + 1), n_ic/(n_ic + 1), num=n_ic)
+    ics = np.zeros((num, num, n_ic, numqB))
+    for i, alpha in enumerate(alphas):
+        for j, rho in enumerate(rhos):
+            for k, w in enumerate(ws):
+                ics[i, j, k] = tt.indifference(icx, w, alpha, rho)
+    norm = Normalize(vmin=0, vmax=1)
 
-width = plotsize*2
-height = plotsize
-xlabel = "Substitutability of $\it{B}$"
-ylabel = "Influence of $\it{B}$"
-biglabel = plotsize*4
-ticklabel = plotsize*3
-xlim=[0.0, 1.0]
-ylim=[0.0, 1.0]
-step = int(num/2)
-plt.rcParams["pdf.fonttype"] = 42
-plt.rcParams["ps.fonttype"] = 42
+    # Figure properties
 
-# Create figure
+    width = plotsize*2
+    height = plotsize
+    xlabel = "Substitutability of $\it{B}$"
+    ylabel = "Influence of $\it{B}$"
+    biglabel = plotsize*4
+    ticklabel = plotsize*3
+    xlim=[0.0, 1.0]
+    ylim=[0.0, 1.0]
+    step = int(num/2)
+    plt.rcParams["pdf.fonttype"] = 42
+    plt.rcParams["ps.fonttype"] = 42
 
-axs = np.empty((2,
-        len(alphas),
-        len(rhos)),
-    dtype=object)
+    # Create figure
 
-fig = plt.figure(figsize=(width, height))
-outergrid = fig.add_gridspec(nrows=1,
-    ncols=2,
-    left=0.15,
-    right=0.85,
-    top=0.8,
-    bottom=0.2)
-for g in range(2):
-    grid = outergrid[g].subgridspec(nrows=num,
-        ncols=num,
-        wspace=0,
-        hspace=0)
-    axs[g] = grid.subplots()
+    axs = np.empty((2,
+            len(alphas),
+            len(rhos)),
+        dtype=object)
 
-left_x = axs[0, 0, 0].get_position().x0
-right_x = axs[-1, -1, -1].get_position().x1
-center_x = (left_x + right_x) / 2
-top_y = axs[0, 0, 0].get_position().y1
-bottom_y = axs[-1, -1, -1].get_position().y0
-center_y = (top_y + bottom_y) / 2
-fig.supxlabel(xlabel,
-    x=center_x,
-    y=bottom_y*0.3,
-    fontsize=biglabel)
-fig.supylabel(ylabel,
-    x=left_x*0.4,
-    y=center_y,
-    fontsize=biglabel)
+    fig = plt.figure(figsize=(width, height))
+    outergrid = fig.add_gridspec(nrows=1,
+        ncols=2,
+        left=0.15,
+        right=0.85,
+        top=0.8,
+        bottom=0.2)
+    for g in range(2):
+        grid = outergrid[g].subgridspec(nrows=num,
+            ncols=num,
+            wspace=0,
+            hspace=0)
+        axs[g] = grid.subplots()
 
-for ax in fig.get_axes():
-    ax.set(xticks=[], yticks=[])
-    ax.set(xlim=xlim, ylim=ylim)
-    for axis in ["top","bottom","left","right"]:
-        ax.spines[axis].set_linewidth(0.2)
+    left_x = axs[0, 0, 0].get_position().x0
+    right_x = axs[-1, -1, -1].get_position().x1
+    center_x = (left_x + right_x) / 2
+    top_y = axs[0, 0, 0].get_position().y1
+    bottom_y = axs[-1, -1, -1].get_position().y0
+    center_y = (top_y + bottom_y) / 2
+    fig.supxlabel(xlabel,
+        x=center_x,
+        y=bottom_y*0.3,
+        fontsize=biglabel)
+    fig.supylabel(ylabel,
+        x=left_x*0.4,
+        y=center_y,
+        fontsize=biglabel)
 
-for g in range(2):
-    letter = ord("a") + g
-    axs[g, 0, 0].set_title(chr(letter),
-        fontsize=plotsize*5,
-        weight="bold",
-        loc="left")
-    if g == 0:
-        for a in range(0, num, step):
-            axs[g, a, 0].set_ylabel(f"{alphas[a]:.1f}",
-                rotation="horizontal",
-                horizontalalignment="right",
-                verticalalignment="center",
+    for ax in fig.get_axes():
+        ax.set(xticks=[], yticks=[])
+        ax.set(xlim=xlim, ylim=ylim)
+        for axis in ["top","bottom","left","right"]:
+            ax.spines[axis].set_linewidth(0.2)
+
+    for g in range(2):
+        letter = ord("a") + g
+        axs[g, 0, 0].set_title(chr(letter),
+            fontsize=plotsize*5,
+            weight="bold",
+            loc="left")
+        if g == 0:
+            for a in range(0, num, step):
+                axs[g, a, 0].set_ylabel(f"{alphas[a]:.1f}",
+                    rotation="horizontal",
+                    horizontalalignment="right",
+                    verticalalignment="center",
+                    fontsize=ticklabel)
+        for r in range(0, num, step):
+            axs[g, -1, r].set_xlabel(f"{logess[r]:.0f}",
                 fontsize=ticklabel)
-    for r in range(0, num, step):
-        axs[g, -1, r].set_xlabel(f"{logess[r]:.0f}",
-            fontsize=ticklabel)
 
-# Assign axs objects to variables
-# (Line2D)
+    # Assign axs objects to variables
+    # (Line2D)
 
-budgets = np.empty_like(axs)
-icurves = np.empty_like(axs)
-dummy_budgety = np.full_like(icx, -1.0)
-dummy_icy = np.zeros_like(icx)
+    budgets = np.empty_like(axs)
+    icurves = np.empty_like(axs)
+    dummy_budgety = np.full_like(icx, -1.0)
+    dummy_icy = np.zeros_like(icx)
 
-for g in range(2):
-    for a, alpha in enumerate(alphas):
-        for r, rho in enumerate(rhos):
-            if g == 0:
-                for c in range(n_ic): 
-                    axs[0, a, r].plot(icx, ics[a, r, c], c="0.850")
-            budgets[g, a, r], = axs[g, a, r].plot(icx,
-                dummy_budgety,
-                c="0.300",
-                linewidth=4,
-                alpha=0.8)
-            icurves[g, a, r], = axs[g, a, r].plot(icx,
-                dummy_icy,
-                linewidth=4,
-                alpha=0.8)
+    for g in range(2):
+        for a, alpha in enumerate(alphas):
+            for r, rho in enumerate(rhos):
+                if g == 0:
+                    for c in range(n_ic): 
+                        axs[0, a, r].plot(icx, ics[a, r, c], c="0.850")
+                budgets[g, a, r], = axs[g, a, r].plot(icx,
+                    dummy_budgety,
+                    c="0.300",
+                    linewidth=4,
+                    alpha=0.8)
+                icurves[g, a, r], = axs[g, a, r].plot(icx,
+                    dummy_icy,
+                    linewidth=4,
+                    alpha=0.8)
 
-# Add colorbar
-axins = inset_axes(axs[0, -1, -1],
-    width="5%",
-    height="100%",
-    loc="upper right",
-    bbox_to_anchor=(880, 200, 200, 200),
-    borderpad=0)
-cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap="Reds"),
-    cax=axins,
-    ticks=[0, 0.5, 1])
-cbar.ax.tick_params(labelsize=ticklabel)
-cbar.outline.set_linewidth(0.2)
+    # Add colorbar
+    axins = inset_axes(axs[0, -1, -1],
+        width="5%",
+        height="100%",
+        loc="upper right",
+        bbox_to_anchor=(880, 200, 200, 200),
+        borderpad=0)
+    cbar = fig.colorbar(cm.ScalarMappable(norm=norm, cmap="Reds"),
+        cax=axins,
+        ticks=[0, 0.5, 1])
+    cbar.ax.tick_params(labelsize=ticklabel)
+    cbar.outline.set_linewidth(0.2)
 
-# Add data and save figure
+    # Add data and save figure
 
-if len(givens) > 1:
-    ani = FuncAnimation(fig,
-        update,
-        frames=givens,
-        fargs=(budgets, icurves,),
-        blit=True)
-    ani.save(f"{file_name}.mp4", writer="ffmpeg", fps=10)
-else:
-    update(givens[0], budgets, icurves,)
-    plt.savefig(f"{file_name}.png", transparent=False)
+    if len(givens) > 1:
+        ani = FuncAnimation(fig,
+            update,
+            frames=givens,
+            fargs=(budgets, icurves, alphas, rhos, icx, budget_0),
+            blit=True)
+        ani.save(f"{file_name}.mp4", writer="ffmpeg", fps=10)
+    else:
+        update(givens[0], budgets, icurves, alphas, rhos, icx, budget_0)
+        plt.savefig(f"{file_name}.png", transparent=False)
 
-plt.close()
+    plt.close()
 
-end_time = time.perf_counter()
-print(f"\nTime elapsed: {(end_time - start_time):.2f} seconds")
+    end_time = time.perf_counter()
+    print(f"\nTime elapsed: {(end_time - start_time):.2f} seconds")
+
+if __name__ == "__main__":
+    main()
