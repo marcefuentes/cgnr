1cb781b685cc277e5294c434e80ade81ca784a8f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-23 22:07:11 +0200

    Update

--- python/images/modules_theory/get_sm.py
+++ python/images/modules_theory/get_sm.py
@@ -12,9 +12,9 @@ from modules.settings import COLOR_MAP
 def get_sm():
     """Create ScalarMappable object using the upper half of the color map."""
 
-    RdBu_r = cm.get_cmap(COLOR_MAP, 256)
-    newcolors = RdBu_r(np.linspace(0, 1, 256))
-    new_map = ListedColormap(newcolors[128:256])
+    original_colors = cm.get_cmap(COLOR_MAP, 256)
+    color_array = original_colors(np.linspace(0, 1, 256))
+    new_map = ListedColormap(color_array[128:256])
     sm = cm.ScalarMappable(cmap=new_map, norm=plt.Normalize(0, 1))
 
     return sm
