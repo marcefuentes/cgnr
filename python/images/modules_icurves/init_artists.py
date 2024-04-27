63ee459f2fb2e5dcd796bf0abeac39357150f49c
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-04-28 01:07:41 +0200

    Update

--- python/images/modules_icurves/init_artists.py
+++ python/images/modules_icurves/init_artists.py
@@ -14,7 +14,6 @@ def init_plot_artists(axs, update_args):
     update_args["budgets"] = np.empty((nr, nc), dtype=object)
     update_args["icurves"] = np.empty_like(update_args["budgets"])
     update_args["landscapes"] = np.empty_like(update_args["budgets"])
-    dummy_budgety = np.full_like(update_args["icx"], -1.0)
     dummy_icy = np.zeros_like(update_args["icx"])
 
     for i in range(nr):
@@ -28,7 +27,7 @@ def init_plot_artists(axs, update_args):
                 )
             (update_args["budgets"][i, j],) = axs[0, 0, i, j].plot(
                 update_args["icx"],
-                dummy_budgety,
+                dummy_icy,
                 c="0.300",
                 lw=get("COMMON", "line_width") * get("COMMON", "plot_size") * 4,
                 alpha=0.8,
