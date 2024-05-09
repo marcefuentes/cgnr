a9019dd790683210899046208b868253faa9842b
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-05-09 09:58:10 +0200

    Update

--- python/images/modules_results/init_artists.py
+++ python/images/modules_results/init_artists.py
@@ -13,17 +13,14 @@ def init_artists_imshow(axs, mr, mc):
 
     dummy_zmatrix = np.zeros((mr, mc))
     cmap = get("COMMON", "color_map")
-    vmin, vmax = -1, 1
 
     for i in range(nrows):
         for j in range(ncols):
             for k in range(nr):
                 for m in range(nc):
-                    ax = axs[i, j, k, m]
-                    artist = ax.imshow(
-                        dummy_zmatrix, cmap=cmap, vmin=vmin, vmax=vmax
+                    artists[i, j, k, m] = axs[i, j, k, m].imshow(
+                        dummy_zmatrix, cmap=cmap, vmin=-1, vmax=1
                     )
-                    artists[i, j, k, m] = artist
     return artists
 
 
@@ -31,7 +28,9 @@ def init_artists_line2d(axs, x, y):
     """Initialize(nrows x ncols x nr x nc) matrix of Line2D artists."""
 
     nrows, ncols, nr, nc = axs.shape
-    artists = np.empty_like(axs)
+    artists = np.empty((nrows, ncols, nr, nc), dtype=object)
+
+    lw = get("COMMON", "line_width") * get("COMMON", "plot_size")
 
     for i in range(nrows):
         for j in range(ncols):
@@ -41,6 +40,6 @@ def init_artists_line2d(axs, x, y):
                         x,
                         y[k, m],
                         c="black",
-                        lw=get("COMMON", "line_width") * get("COMMON", "plot_size"),
+                        lw=lw,
                     )
     return artists
