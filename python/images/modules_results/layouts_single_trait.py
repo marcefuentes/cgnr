a0db9a70dcdbdd80dd3c796c7d9991f1a042ad80
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-05-22 10:55:00 +0200

    Update

---
+++ python/images/modules_results/layouts_single_trait.py
@@ -0,0 +1,14 @@
+""" Columns and rows for a single trait """
+
+columns = ["nolang_noshuffle", "lang_noshuffle", "nolang_shuffle", "lang_shuffle"]
+
+variant_suffixes = [
+    "cost15_128",
+    "cost15_128",
+    "cost15_128",
+    "cost15_4",
+    "cost15_4",
+    "cost15_4",
+]
+
+rows = ["p", "pi", "i", "p", "pi", "i"]
