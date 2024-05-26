4679ad65b44f4b8294688462d21767a574364700
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-05-26 17:27:57 +0200

    Update

--- python/images/modules/settings.py
+++ python/images/modules/settings.py
@@ -1,11 +1,11 @@
 """ Parameters for the images module. """
 
-PLOT_SIZE = 4
+PLOT_SIZE = 8
 
 SETTINGS = {
     "big_label_size": 9 * PLOT_SIZE,
     "border_color": "0.3",
-    "border_width": 0.05 * PLOT_SIZE,
+    "border_width": 0.1 * PLOT_SIZE,
     "bottom_margin": 0.625 * PLOT_SIZE,
     "color_map": "RdBu_r",
     "left_margin": 0.625 * PLOT_SIZE,
@@ -15,9 +15,9 @@ SETTINGS = {
     "plot_size": PLOT_SIZE,
     "right_margin": 1 * PLOT_SIZE,
     "spacing": 0.1875 * PLOT_SIZE,
-    "tick_color": "0.8",
+    "tick_color": "0.3",
     "tick_label_size": 6 * PLOT_SIZE,
-    "tick_size": 1.5 * PLOT_SIZE,
+    "tick_size": 2.0 * PLOT_SIZE,
     "title_padding": 11,
     "top_margin": 0.625 * PLOT_SIZE,
     "x_label": "Substitutability of $\\it{B}$",
