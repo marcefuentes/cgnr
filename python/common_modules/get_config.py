f70d2467f5d9334220d3cce43b5b6030d9b998d7
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-05-26 11:34:25 +0200

    Update

--- python/common_modules/get_config.py
+++ python/common_modules/get_config.py
@@ -4,28 +4,7 @@ import configparser
 import os
 
 from common_modules import color
-
-VARIABLE_TYPES = {
-    "Cost": "int",
-    "DeathRate": "int",
-    "Given": "float",
-    "GrainMutationSize": "int",
-    "GroupSize": "int",
-    "N": "int",
-    "Periods": "int",
-    "Runs": "int",
-    "alpha_max": "float",
-    "alpha_min": "float",
-    "bins": "int",
-    "constraints": "list",
-    "grid": "int",
-    "hours": "int",
-    "loges_max": "float",
-    "loges_min": "float",
-    "number_of_lines": "int",
-    "output_file_extensions": "list",
-    "qBMutationSize": "int",
-}
+from config_ini_types import VARIABLE_TYPES
 
 
 def get_config(variable):
---
+++ python/config_ini_types.py
@@ -0,0 +1,23 @@
+""" Configuration variables for the project. """
+
+VARIABLE_TYPES = {
+    "Cost": "int",
+    "DeathRate": "int",
+    "Given": "float",
+    "GrainMutationSize": "int",
+    "GroupSize": "int",
+    "N": "int",
+    "Periods": "int",
+    "Runs": "int",
+    "alpha_max": "float",
+    "alpha_min": "float",
+    "bins": "int",
+    "constraints": "list",
+    "grid": "int",
+    "hours": "int",
+    "loges_max": "float",
+    "loges_min": "float",
+    "number_of_lines": "int",
+    "output_file_extensions": "list",
+    "qBMutationSize": "int",
+}
