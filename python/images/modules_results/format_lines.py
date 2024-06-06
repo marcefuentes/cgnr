9e8941df27c2fd892a2558caac1cb06273df7d4a
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-06 18:28:33 +0200

    Update

---
+++ python/images/modules_results/format_lines.py
@@ -0,0 +1,14 @@
+""" Format lines. """
+
+
+def format_lines(lines, image):
+    """ Format lines. """
+
+    for i in range(lines.shape[0]):
+        for j in range(lines.shape[1]):
+            for k in range(lines.shape[2]):
+                for m in range(lines.shape[3]):
+                    line = lines[i, j, k, m]
+                    line.set_color("0.3")
+                    line.set_linewidth(image["line_width"] * image["plot_size"])
+
