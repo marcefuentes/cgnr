0f07b495f96697e0af098119506a26bb443b4b03
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-11 09:41:01 +0200

    Update

--- python/images/modules_results/get_curves.py
+++ python/images/modules_results/get_curves.py
@@ -1,4 +1,4 @@
-""" Calculates fitness isoclines. """
+""" Calculates fitness curves. """
 
 import numpy as np
 from modules.theory import calculate_trps, qbeq
@@ -10,16 +10,12 @@ def get_curves(num, traits, givens, alphas, rhos):
     x = np.linspace(0.001, 0.999, num=num)
     y = np.zeros((len(givens), len(givens[0]), len(alphas), len(rhos), len(x)))
 
-    for i in range(y.shape[0]):
-        for j in range(y.shape[1]):
-            for k in range(y.shape[2]):
-                for m in range(y.shape[3]):
-                    y[i, j, k, m] = process_plot(
-                        x, traits[i][j], float(givens[i][j]), alphas[k], rhos[m]
-                    )
+    for i, j, k, m in np.ndindex(y.shape[:-1]):
+        y[i, j, k, m] = process_plot(
+            x, traits[i][j], float(givens[i][j]), alphas[k], rhos[m]
+        )
 
-    mask = y <= 0
-    y[mask] = np.nan
+    y[y <= 0] = np.nan
 
     return x, y
 
@@ -29,6 +25,7 @@ def process_plot(x, trait, given, alpha, rho):
 
     increment = 0.001
     tt, rr, pp, ss = calculate_trps(x + increment, x, given, alpha, rho)
+
     if trait == "MimicGrain":
         y = pp - ss
         # y = (pp - ss) / (64 * rr - ss + 2 * pp - tt - 64 * pp)
@@ -37,6 +34,6 @@ def process_plot(x, trait, given, alpha, rho):
 
     y *= 500
     mask = (x + increment < qbeq(given, alpha, rho)) | (x > qbeq(0.0, alpha, rho))
-    y[mask] = None
+    y[mask] = np.nan
 
     return y
