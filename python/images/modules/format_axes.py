4824c5b6e91b6e130350c22cfb2f62fe81167440
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-11 10:13:57 +0200

    Update

--- python/images/modules/format_axes.py
+++ python/images/modules/format_axes.py
@@ -10,6 +10,14 @@ def format_axes(axes_args, image):
     axs = axes_args["axs"]
     nrows, ncols, nr, nc = axs.shape
 
+    # Position axes
+
+    for i, j, k, m in np.ndindex(axs.shape):
+        axs[i, j, k, m].set(axes_locator = axes_args["divider"].new_locator(
+            nx=j * (nc + 1) + m + int(m / nc),
+            ny=(nrows - i - 1) * (nr + 1) + nr - k - int(k / nr) - 1,
+        ))
+
     # Format spines
 
     params = {
@@ -19,20 +27,30 @@ def format_axes(axes_args, image):
     for ax in axs.flatten():
         tools.format_spines(ax, **params)
 
-    # Reset ticks, set limits and position axes
+    # Set limits and reset ticks
 
     params = {
-        "xticks": [],
-        "yticks": [],
         "xlim": axes_args["x_lim"],
         "ylim": axes_args["y_lim"],
+        "xticks": [],
+        "yticks": [],
     }
-    for i, j, k, m in np.ndindex(axs.shape):
-        params["axes_locator"] = axes_args["divider"].new_locator(
-            nx=j * (nc + 1) + m + int(m / nc),
-            ny=(nrows - i - 1) * (nr + 1) + nr - k - int(k / nr) - 1,
-        )
-        axs[i, j, k, m].set(**params)
+    for ax in axs.flatten():
+        ax.set(**params)
+
+    # Add ticks and tick labels
+
+    params = {
+        "labelsize": image["tick_label_size"],
+        "size": image["tick_size"],
+        "color": image["tick_color"],
+    }
+    if axs.shape[2] == 1:
+        tools.add_ticks_imshow(axs, axes_args["nr"], axes_args["nc"], params)
+        tools.add_ticklabels_imshow(axs, axes_args["r_labels"], axes_args["c_labels"])
+    else:
+        tools.add_ticks_line2d(axs, params)
+        tools.add_ticklabels_line2d(axs, axes_args["r_labels"], axes_args["c_labels"])
 
     # Add letters
 
@@ -70,17 +88,3 @@ def format_axes(axes_args, image):
     }
     for i in range(nrows):
         axs[i, -1, int(nr / 2), -1].annotate(axes_args["row_titles"][i], **params)
-
-    # Add ticks and tick labels
-
-    params = {
-        "labelsize": image["tick_label_size"],
-        "size": image["tick_size"],
-        "color": image["tick_color"],
-    }
-    if axs.shape[2] == 1:
-        tools.add_ticks_imshow(axs, axes_args["nr"], axes_args["nc"], params)
-        tools.add_ticklabels_imshow(axs, axes_args["r_labels"], axes_args["c_labels"])
-    else:
-        tools.add_ticks_line2d(axs, params)
-        tools.add_ticklabels_line2d(axs, axes_args["r_labels"], axes_args["c_labels"])
