00b6e77307d2bb5264c895db7afb36c214893ae9
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-12 12:38:18 +0200

    Update

--- python/images/settings_icurves/data_constants.py
+++ python/images/settings_icurves/data_constants.py
@@ -6,34 +6,32 @@ import numpy as np
 def data_constants():
     """Data common to all subplots."""
 
-    given_params = {
+    given = {
         "start": 0.0,
         "stop": 1.0,
         "num": 11,
     }
 
-    alpha_params = {
+    alpha = {
         "start": 0.1,
         "stop": 0.9,
         "num": 3,
     }
 
-    loges_params = {
+    loges = {
         "start": -2.0,
         "stop": 2.0,
         "num": 3,
     }
 
     data = {
-        "alphas": np.linspace(**alpha_params),
+        "alphas": np.linspace(**alpha),
+        "frames": np.concatenate([np.linspace(**given), [0.0]])
         "givens": [1.0, 0.99, 0.5, 0.0],
-        "logess": np.linspace(**loges_params),
+        "logess": np.linspace(**loges),
         "n_ic": 5,
         "n_x_values": 64,
     }
-
-    data["frames"] = np.linspace(**given_params)
-    data["frames"] = np.append(data["frames"], 0.0)
 
     data["rhos"] = 1.0 - 1.0 / np.power(2.0, data["logess"])
 
