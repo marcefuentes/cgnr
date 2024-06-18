3dbebc247fa8fd98ca6d00ab1fe933a789205c03
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-18 13:44:14 +0200

    Update

---
+++ python/images/resultsm/adjust.py
@@ -0,0 +1,12 @@
+from modules.add_ticks import add_ticklabels_imshow_1
+
+def adjust(axs):
+
+    axs[1, 0, 0, 0].remove()
+    ax = axs[0, 0, 0, 0]
+    l, b, w, h = ax.get_position().bounds
+    ax.set_axes_locator(None)
+    new_position = [l + 0.04833, b * 0.667, w * 0.833, h *0.833]
+    ax.set_position(new_position)
+    ax.set_title("No shuffling", fontsize=32, pad=215)
+    add_ticklabels_imshow_1(ax, [0.1, 0, 0.9], [-5, 0, 5])
