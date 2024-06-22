fb83f7158b4f5738f83f7cba26adb9c3fad13b84
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-06-22 20:19:21 +0200

    Update

--- python/images/modules/enlarge_ticks.py
+++ python/images/modules/enlarge_ticks.py
@@ -6,78 +6,60 @@ def enlarge_ticks(axs, options, margin_inner, linewidth):
 
     if options["layout"] == "allp":
         axs_to_change = []
-        if options["lang"]:
-            if "Choose" in options["trait"]:
-                axs_to_change = [
-                    (axs[1, 0, 0, 0], "x"),
-                    (axs[1, 1, 0, 0], "y"),
-                    (axs[1, 1, 0, 0], "x"),
-                    (axs[2, 1, 0, 0], "y"),
-                    (axs[1, 2, 0, 0], "y"),
-                    (axs[1, 2, 0, 0], "x"),
-                    (axs[1, 3, 0, 0], "y"),
-                    (axs[1, 3, 0, 0], "x"),
-                    (axs[2, 2, 0, 0], "y"),
-                    (axs[2, 3, 0, 0], "y"),
-                ]
-            elif options["trait"] == "MimicGrainmean":
-                axs_to_change = [
-                    (axs[0, 1, 0, 0], "y"),
-                    (axs[0, 2, 0, 0], "y"),
-                    (axs[0, 3, 0, 0], "y"),
-                    (axs[1, 1, 0, 0], "y"),
-                    (axs[2, 1, 0, 0], "y"),
-                ]
-            elif "Imimic" in options["trait"]:
-                axs_to_change = [
-                    (axs[0, 0, 0, 0], "x"),
-                    (axs[0, 1, 0, 0], "y"),
-                    (axs[0, 1, 0, 0], "x"),
-                    (axs[0, 2, 0, 0], "y"),
-                    (axs[0, 2, 0, 0], "x"),
-                    (axs[0, 3, 0, 0], "y"),
-                    (axs[0, 3, 0, 0], "x"),
-                    (axs[1, 1, 0, 0], "y"),
-                    (axs[1, 2, 0, 0], "y"),
-                    (axs[1, 3, 0, 0], "y"),
-                    (axs[2, 1, 0, 0], "y"),
-                    (axs[3, 1, 0, 0], "y"),
-                    (axs[3, 2, 0, 0], "y"),
-                    (axs[3, 3, 0, 0], "y"),
-                ]
-        else:
+        if "Choose" in options["trait"]:
             axs_to_change = [
                 (axs[1, 0, 0, 0], "x"),
                 (axs[1, 1, 0, 0], "y"),
                 (axs[1, 1, 0, 0], "x"),
                 (axs[2, 1, 0, 0], "y"),
+                (axs[1, 2, 0, 0], "y"),
+                (axs[1, 2, 0, 0], "x"),
+                (axs[1, 3, 0, 0], "y"),
+                (axs[1, 3, 0, 0], "x"),
+                (axs[2, 2, 0, 0], "y"),
+                (axs[2, 3, 0, 0], "y"),
             ]
-            if options["trait"] == "ChooseGrainmean":
-                    axs_to_change.append((axs[1, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[1, 2, 0, 0], "x"))
-                    axs_to_change.append((axs[1, 3, 0, 0], "y"))
-                    axs_to_change.append((axs[1, 3, 0, 0], "x"))
-                    axs_to_change.append((axs[2, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[2, 3, 0, 0], "y"))
-            elif options["trait"] == "MimicGrainmean":
-                    axs_to_change.append((axs[0, 1, 0, 0], "y"))
-                    axs_to_change.append((axs[0, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[0, 3, 0, 0], "y"))
-            elif options["trait"] == "ImimicGrainmean":
-                    axs_to_change.append((axs[0, 0, 0, 0], "x"))
-                    axs_to_change.append((axs[0, 1, 0, 0], "y"))
-                    axs_to_change.append((axs[0, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[0, 3, 0, 0], "y"))
-                    axs_to_change.append((axs[0, 1, 0, 0], "x"))
-                    axs_to_change.append((axs[0, 2, 0, 0], "x"))
-                    axs_to_change.append((axs[0, 3, 0, 0], "x"))
-                    axs_to_change.append((axs[1, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[1, 3, 0, 0], "y"))
-                    axs_to_change.append((axs[2, 0, 0, 0], "x"))
-                    axs_to_change.append((axs[2, 1, 0, 0], "x"))
-                    axs_to_change.append((axs[3, 1, 0, 0], "y"))
-                    axs_to_change.append((axs[3, 2, 0, 0], "y"))
-                    axs_to_change.append((axs[3, 3, 0, 0], "y"))
+        elif options["trait"] == "MimicGrainmean":
+            axs_to_change = [
+                (axs[0, 1, 0, 0], "y"),
+                (axs[0, 2, 0, 0], "y"),
+                (axs[0, 3, 0, 0], "y"),
+                (axs[1, 1, 0, 0], "y"),
+                (axs[2, 1, 0, 0], "y"),
+            ]
+            if not options["lang"]:
+                axs_to_change.append((axs[1, 0, 0, 0], "x"))
+                axs_to_change.append((axs[1, 1, 0, 0], "x"))
+        elif "Imimic" in options["trait"]:
+            axs_to_change = [
+                (axs[0, 0, 0, 0], "x"),
+                (axs[0, 1, 0, 0], "y"),
+                (axs[0, 1, 0, 0], "x"),
+                (axs[0, 2, 0, 0], "y"),
+                (axs[0, 2, 0, 0], "x"),
+                (axs[0, 3, 0, 0], "y"),
+                (axs[0, 3, 0, 0], "x"),
+                (axs[1, 1, 0, 0], "y"),
+                (axs[1, 2, 0, 0], "y"),
+                (axs[1, 3, 0, 0], "y"),
+                (axs[2, 1, 0, 0], "y"),
+                (axs[3, 1, 0, 0], "y"),
+                (axs[3, 2, 0, 0], "y"),
+                (axs[3, 3, 0, 0], "y"),
+            ]
+            if not options["lang"]:
+                axs_to_change.append((axs[1, 0, 0, 0], "x"))
+                axs_to_change.append((axs[1, 1, 0, 0], "x"))
+                axs_to_change.append((axs[2, 0, 0, 0], "x"))
+                axs_to_change.append((axs[2, 1, 0, 0], "x"))
+        else:
+            axs_to_change = [
+                (axs[1, 1, 0, 0], "y"),
+                (axs[2, 1, 0, 0], "y"),
+            ]
+            if not options["lang"]:
+                axs_to_change.append((axs[1, 0, 0, 0], "x"))
+                axs_to_change.append((axs[1, 1, 0, 0], "x"))
 
         for ax in axs_to_change:
             if ax[1] == "x":
