69958f9eba2f231c2b01702a24c14b6d65c5072f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-07-14 12:29:10 +0200

    Update

--- python/images/resultsm/adjust.py
+++ python/images/resultsm/adjust.py
@@ -10,8 +10,6 @@ def adjust(axs):
     ax = axs[0, 0, 0, 0]
     l, b, w, h = ax.get_position().bounds
     ax.set_axes_locator(None)
-    # new_position = [l - 0.035, b * 0.603, w * 1.05, h * 1.05]
-    new_position = [l + 0.04833, b * 0.667, w * 0.833, h * 0.833]
-    ax.set_position(new_position)
+    ax.set_position((0.133, 0.3443, 0.213, 0.315))
     ax.set_title("No shuffling\n$\\mathit{s}_{\\mathit{1}}$, $\\mathit{s}_{\\mathit{3}}$", fontsize=32, pad=215)
     add_ticklabels_ax(ax, [0.1, 0, 0.9], [-31, 0, 0.97])
