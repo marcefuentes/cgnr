f5eb7bc171b2072d33c928905c09b4028ea312b1
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-07-16 09:24:12 +0200

    Update

---
+++ python/images/resultss/layouts/repeat_for_matrix.py
@@ -0,0 +1,7 @@
+"""Repeat a value for a matrix."""
+
+
+def repeat_for_matrix(value, nrows, ncols):
+    """Repeat a value for a matrix."""
+
+    return [[value for _ in range(ncols)] for _ in range(nrows)]
