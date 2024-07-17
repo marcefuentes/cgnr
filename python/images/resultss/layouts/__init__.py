73696c243c8d4bb271537ff1a123b3e7b15d0435
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-07-17 19:24:30 +0200

    Update

--- python/images/resultss/layouts/__init__.py
+++ python/images/resultss/layouts/__init__.py
@@ -4,14 +4,10 @@ from .curves import curves
 from .dilemmas import dilemmas
 from .m01 import m01
 from .m02 import m02
-from .m03 import m03
+from .m03_adjust import m03_adjust
 from .m04 import m04
-from .m05 import m05
-from .m06 import m06
+from .m06_adjust import m06_adjust
 from .m08 import m08
-from .m10 import m10
-from .m16 import m16
-from .m24 import m24
 from .s1_i_adjust import s1_i_adjust
 from .s1_pi import s1_pi
 from .theory import theory
@@ -22,14 +18,10 @@ __all__ = [
     "dilemmas",
     "m01",
     "m02",
-    "m03",
+    "m03_adjust",
     "m04",
-    "m05",
     "m06",
     "m08",
-    "m10",
-    "m16",
-    "m24",
     "s1_i_adjust",
     "s1_pi",
     "theory",
