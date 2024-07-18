1996e36b2910eb632488000540344043f5457c7d
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-07-18 11:41:19 +0200

    Update

--- python/images/resultss/layouts/fig5.py
+++ python/images/resultss/layouts/fig5.py
@@ -3,12 +3,21 @@
 from .default_data import default_data
 from .ss import S1, S2
 
+
 def fig5(data):
     """Figure 5."""
 
     variants = [
-        [f"nolang_shuffle_cost15_128", f"nolang_shuffle_cost15_128", f"nolang_shuffle_cost15_128"],
-        [f"nolang_shuffle_cost15_4", f"nolang_shuffle_cost15_4", f"nolang_shuffle_cost15_4"],
+        [
+            f"nolang_shuffle_cost15_128",
+            f"nolang_shuffle_cost15_128",
+            f"nolang_shuffle_cost15_128",
+        ],
+        [
+            f"nolang_shuffle_cost15_4",
+            f"nolang_shuffle_cost15_4",
+            f"nolang_shuffle_cost15_4",
+        ],
     ]
 
     nrows = len(variants)
@@ -18,7 +27,9 @@ def fig5(data):
     data["givens_control"] = [["1.0", "1.0", "1.0"] for _ in range(nrows)]
     data["mechanisms"] = [["i", "i", "i"] for _ in range(nrows)]
     data["titles_columns"] = [S1, S2, "Fitness"]
-    data["traits"] = [["MimicGrainmean", "ImimicGrainmean", "wmean"] for _ in range(nrows)]
+    data["traits"] = [
+        ["MimicGrainmean", "ImimicGrainmean", "wmean"] for _ in range(nrows)
+    ]
     data["traits_control"] = data["traits"]
 
     return data
--- python/images/resultss/layouts/fig6.py
+++ python/images/resultss/layouts/fig6.py
@@ -3,12 +3,23 @@
 from .default_data import default_data
 from .ss import S3, S5
 
+
 def fig6(data):
     """Figure 6."""
 
     variants = [
-        [f"lang_shuffle_cost15_128", f"lang_shuffle_cost15_128", f"lang_shuffle_cost15_128", f"lang_shuffle_cost15_128"],
-        [f"lang_shuffle_cost15_4", f"lang_shuffle_cost15_4", f"lang_shuffle_cost15_4", f"lang_shuffle_cost15_4"],
+        [
+            f"lang_shuffle_cost15_128",
+            f"lang_shuffle_cost15_128",
+            f"lang_shuffle_cost15_128",
+            f"lang_shuffle_cost15_128",
+        ],
+        [
+            f"lang_shuffle_cost15_4",
+            f"lang_shuffle_cost15_4",
+            f"lang_shuffle_cost15_4",
+            f"lang_shuffle_cost15_4",
+        ],
     ]
 
     nrows = len(variants)
@@ -18,7 +29,10 @@ def fig6(data):
     data["givens_control"] = [["1.0", "1.0", "0.0", "0.0"] for _ in range(nrows)]
     data["mechanisms"] = [["pi", "pi", "pi", "pi"] for _ in range(nrows)]
     data["titles_columns"] = [S5, S3, "Production of $\\it{B}$", "Fitness"]
-    data["traits"] = [["Choose_ltGrainmean", "Imimic_ltGrainmean", "qBSeenmean", "wmean"] for _ in range(nrows)]
+    data["traits"] = [
+        ["Choose_ltGrainmean", "Imimic_ltGrainmean", "qBSeenmean", "wmean"]
+        for _ in range(nrows)
+    ]
     data["traits_control"] = data["traits"]
 
     return data
