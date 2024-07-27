286a1e393ea5a0af51eb82a064aab1cf5aee570b
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-07-27 12:56:55 +0200

    Update

--- python/images/resultss/layouts/fig7.py
+++ python/images/resultss/layouts/fig7.py
@@ -1,19 +1,29 @@
 """Show how adding cooperation mechanisms contributes to aaleviating social dilemmas."""
 
-from .default_data import default_data_ipi, get_subtitles
+from .default_data import default_data, get_subtitles
 
 
 def fig7(data):
     """Plots for partner choice plus reciprocity."""
 
-    mechanisms = ["p", "pi", "p", "pi"]
-    variants_common = [
-        f"nolang_noshuffle_cost15",
-        f"lang_noshuffle_cost15",
-        f"nolang_shuffle_cost15",
-        f"lang_shuffle_cost15",
+    mechanisms= ["p", "pi", "p", "pi"]
+    variants = [
+        [
+            "nolang_noshuffle_cost15_128",
+            "lang_noshuffle_cost15_128",
+            "nolang_shuffle_cost15_128",
+            "lang_shuffle_cost15_128",
+        ],
+        [
+            "nolang_noshuffle_cost15_4",
+            "lang_noshuffle_cost15_4",
+            "nolang_shuffle_cost15_4",
+            "lang_shuffle_cost15_4",
+        ],
     ]
-    default_data_ipi(data, mechanisms, variants_common)
+    default_data(data, variants)
+    data["mechanisms"] = [mechanisms for _ in range(len(variants))]
+    data["titles_columns"] = ["No shuffling", "No shuffling", "Shuffling", "Shuffling"]
     data["titles_columns"] = get_subtitles(
-        data["titles_columns"], variants_common, mechanisms
+        data["titles_columns"], variants[0], mechanisms
     )
