b33dd9fe72d565e1f3b102d86e24a3eb437f96ec
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-02 13:50:38 +0200

    Update

--- python/images/resultss/layouts/fill_matrix.py
+++ python/images/resultss/layouts/fill_matrix.py
@@ -1,7 +1,7 @@
 """Repeat a value for a matrix."""
 
 
-def fill_matrix(value, variants):
+def fill_matrix(value, matrix):
     """Repeat a value for a matrix."""
 
-    return [[value for _ in row] for row in variants]
+    return [[value for _ in row] for row in matrix]
