85c067f15798c0e77682cb920ca0b3a2cf7c47ed
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-04 12:42:21 +0200

    Update

--- python/images/modules/artists_format.py
+++ python/images/modules/artists_format.py
@@ -1,10 +1,10 @@
 """ Format artists. """
 
-import numpy as np
+from numpy import ndindex
 
 
 def artists_format(artists, params):
     """Format artists."""
 
-    for idx in np.ndindex(artists.shape):
+    for idx in ndindex(artists.shape):
         artists[idx].set(**params)
