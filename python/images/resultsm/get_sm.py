a2bc1762910f08abc3a7b56e06f96e36412dc3e2
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-04 11:59:20 +0200

    Update

--- python/images/resultsm/get_sm.py
+++ python/images/resultsm/get_sm.py
@@ -1,12 +1,12 @@
 """Create ScalarMappable object for color mapping."""
 
 from matplotlib.cm import ScalarMappable
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import Normalize
 
 
 def get_sm(color_map):
     """Create ScalarMappable object for color mapping."""
 
-    sm = ScalarMappable(cmap=color_map, norm=plt.Normalize(-1, 1))
+    sm = ScalarMappable(cmap=color_map, norm=Normalize(-1, 1))
 
     return sm
