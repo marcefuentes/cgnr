6bf2c5146fad0e6c7237f3b53396b7486576f65f
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-04 12:48:16 +0200

    Update

--- python/images/icurvesm/get_sm.py
+++ python/images/icurvesm/get_sm.py
@@ -1,6 +1,6 @@
 """Create ScalarMappable object for color mapping."""
 
-import numpy as np
+from numpy import linspace
 
 from matplotlib.cm import ScalarMappable
 from matplotlib.colors import ListedColormap
@@ -10,7 +10,7 @@ from matplotlib.pyplot import Normalize
 def get_sm(color_map):
     """Create ScalarMappable object using the upper half of the color map."""
 
-    color_array = color_map(np.linspace(0, 1, 256))
+    color_array = color_map(linspace(0, 1, 256))
     new_map = ListedColormap(color_array[128:256])
     sm = ScalarMappable(cmap=new_map, norm=Normalize(0, 1))
 
