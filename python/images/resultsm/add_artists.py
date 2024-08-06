ea0004d8298911f2e1cf70929ec5589ca8d70d02
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-06 18:05:45 +0200

    Update

--- python/images/resultsm/add_artists.py
+++ python/images/resultsm/add_artists.py
@@ -8,18 +8,15 @@ def add_artists(data, image):
 
     axs = image["axs"]
     artists = np.empty_like(axs, dtype=object)
-    y = data["y"]
 
     for idx in np.ndindex(axs.shape):
+        ax = axs[idx]
+        y = data["y"][idx]
         if data["ax_type"] == "AxesImage":
-            artists[idx] = axs[idx].imshow(y[idx], **image["AxesImage"])
+            artists[idx] = ax.imshow(y, **image["AxesImage"])
         elif data["ax_type"] == "Line2D":
-            artists[idx] = axs[idx].plot(data["x"], y[idx], **image["Line2D"])[
-                0
-            ]
+            artists[idx] = ax.plot(data["x"], y, **image["Line2D"])[0]
         else:
-            artists[idx] = axs[idx].fill_between(
-                data["x"], 0, y[idx], **image["PolyCollection"]
-            )
+            artists[idx] = ax.fill_between(data["x"], 0, y, **image["PolyCollection"])
 
     data["artists"] = artists
