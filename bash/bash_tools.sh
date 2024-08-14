7f436c285556fc7208b132e8b396e3a89a7e7680
Marcelino Fuentes <marcelinofuentes@gmail.com>
2024-08-14 17:52:02 +0200

    Update

--- bash/bash_tools.sh
+++
@@ -1,6 +0,0 @@
-tail -n 1 *.out
-find . -name \*.out -type f -delete
-find . -type f -name "*.py" -print0 | xargs -0 sed -i 's/given/g/g'
-find . -name '*.csv' | xargs wc -l # number of lines
-find . -type f -name "101.csv" -exec awk 'FNR == 1 && /Imimic/ {print FILENAME; nextfile}' {} + # find file with specific string
-rsync -avm --include='*.glo' -f 'hide,! */' continuous/* lcontinuous/
