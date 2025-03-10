453d29c851d275d16cd0e6a2b20ce112c5ec0e44
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-03-10 17:04:22 +0100

    Update

---
+++ code/src/include/globals.h
@@ -0,0 +1,30 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+struct GlobalVariables {
+    int           seed;
+    unsigned int  population_size;
+    unsigned int  runs;
+    unsigned long time;
+    unsigned int  periods;
+    double        qb_mutation_size;
+    double        grain_mutation_size;
+    double        death_rate;
+    unsigned int  group_size;
+    double        cost;
+    int           partner_choice;
+    int           reciprocity;
+    int           indirect_r;
+    int           language;
+    int           shuffle;
+    double        alpha;
+    double        loges;
+    double        given;
+    double        rho;
+};
+
+extern struct GlobalVariables globals;
+
+int read_globals(const char *filename);
+
+#endif  // CONFIG_H
