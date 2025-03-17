48fe2b4a37f6ae7e42da806ad4143148cbc84fcc
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-03-17 21:45:27 +0100

    Update

--- code/src/main.c
+++ code/src/main.c
@@ -137,7 +137,7 @@ int simulation(struct Stats *stats, char *filename) {
             stats[period].logES = globals.loges;
             stats[period].Given = globals.given;
             stats[period].time = time + 1;
-            stats_period(ind_first, ind_last, stats + period, globals.population_size);
+            stats_period(ind_first, ind_last, &stats[period], globals.population_size);
             if (globals.runs == 1) {
                 write_ics(filename, period, (float)globals.alpha, (float)globals.loges, (float)globals.given, time + 1,
                           ind_first, ind_last);
