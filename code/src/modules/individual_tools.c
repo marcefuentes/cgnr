b8933ee0710e345ca9c88718ffae5a8db2ec37e5
Marcelino Fuentes <marcelinofuentes@gmail.com>
2025-03-19 16:28:17 +0100

    Update

--- code/src/modules/individual_tools.c
+++ code/src/modules/individual_tools.c
@@ -7,3 +7,18 @@ void initial_pairs(Individual *ind_first, Individual *ind_last) {
         ind_j->partner = ind_i;
     }
 }
+
+void initial_pairs2(Individual *ind_first[], Individual *ind_last[]) {
+    Individual *ind_j = ind_first[1];
+    for (Individual *ind_i = ind_first[0]; ind_i < ind_last[0] && ind_j < ind_last[1]; ind_i++, ind_j++) {
+        ind_i->partner = ind_j;
+        ind_j->partner = ind_i;
+    }
+}
+
+void update_scores(Individual *ind_first, Individual *ind_last) {
+    for (Individual *ind = ind_first; ind < ind_last; ind++) {
+        ind->qBSeenSum += ind->qBSeen;
+        ind->qBSeen_lt = ind->qBSeenSum / ind->age;
+    }
+}
--- code/src/modules/update_scores.c
+++
@@ -1,8 +0,0 @@
-#include "individual.h"
-
-void update_scores(Individual *ind_first, Individual *ind_last) {
-    for (Individual *ind = ind_first; ind < ind_last; ind++) {
-        ind->qBSeenSum += ind->qBSeen;
-        ind->qBSeen_lt = ind->qBSeenSum / ind->age;
-    }
-}
